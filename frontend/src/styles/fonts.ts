c5c6bc7a93892b125ac3753e8ceab0c7fd88c601
Adhisafdhol <aadheusx@gmail.com>
2025-02-06 17:17:47 +0700

    Add and set up ibm plex mono assets

--- frontend/src/styles/fonts.ts
+++ frontend/src/styles/fonts.ts
@@ -13,4 +13,21 @@ const inter = localFont({
   fallback: ["system-ui", "sans-serif"],
 });
 
-export { inter };
+const ibmPlexMono = localFont({
+  src: [
+    {
+      path: "../../public/fonts/ibm-plex-mono/ibmplexmono-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ibm-plex-mono/ibmplexmono-medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-ibm-plex-mono",
+  fallback: ["system-ui", "monospace"],
+});
+
+export { inter, ibmPlexMono };
