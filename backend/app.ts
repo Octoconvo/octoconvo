8c625b4edadb5f245d43723db8625a523d5f717b
Adhisafdhol <aadheusx@gmail.com>
2025-02-11 21:59:38 +0700

    Set up passportJS with prismaSessionStore

--- backend/app.ts
+++ backend/app.ts
@@ -3,9 +3,31 @@ import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index";
 import userRouter from "./routes/user";
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
+import passportConfig from "./config/passportConfig";
 
 const app = express();
 app.use(logger("dev"));
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    },
+    secret: process.env.SECRET as string,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, // 2 minutes,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+app.use(passportConfig.session());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
