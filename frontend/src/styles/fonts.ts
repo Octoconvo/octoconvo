e47a490bac270fb18fc64321f83d64e34b2a4f90
Adhisafdhol <aadheusx@gmail.com>
2025-02-13 00:28:10 +0700

    Replace inter font file and use woff2 only
    Replace Inter with the file from inter official website to fix
    off-centered vertical metrics.

--- frontend/src/styles/fonts.ts
+++ frontend/src/styles/fonts.ts
@@ -5,9 +5,6 @@ const inter = localFont({
     {
       path: "../../public/fonts/inter/inter-v.woff2",
     },
-    {
-      path: "../../public/fonts/inter/inter-v.woff",
-    },
   ],
   variable: "--font-inter",
   fallback: ["system-ui", "sans-serif"],
