5b483b484bf2fe1ed23db9b1950a1b80990defa5
Adhisafdhol <aadheusx@gmail.com>
2025-02-14 00:23:45 +0700

    Rename signup test file to signupform
    Move signup page test file to component signupForm test file to improve
    separation of concerns. The form inside signup page will be separated
    into its own module, so the signup test file which currently only test
    element inside the form will be renamed and moved into
    signupForm.test.tsx to reflect the change.

--- frontend/__test__/components/signupForm.test.tsx
+++ frontend/__test__/components/signupForm.test.tsx
@@ -1,10 +1,16 @@
-import SignupPage from "@/app/signup/page";
+import SignupForm from "@/components/SignupForm";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {};
+  },
+}));
+
 describe("Render signup page", () => {
   beforeEach(() => {
-    render(<SignupPage />);
+    render(<SignupForm />);
   });
 
   it("Check whether username input is rendered", async () => {
