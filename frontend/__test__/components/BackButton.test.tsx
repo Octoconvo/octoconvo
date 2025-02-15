594f081ebe26accca3b7b67be7ffa4b13623487b
Adhisafdhol <aadheusx@gmail.com>
2025-02-15 20:47:21 +0700

    Use useRouter in BackButton component

--- frontend/__test__/components/BackButton.test.tsx
+++ frontend/__test__/components/BackButton.test.tsx
@@ -3,6 +3,14 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 
+jest.mock("next/navigation", () => ({
+  useRouter() {
+    return {
+      back: jest.fn(() => window.history.back()),
+    };
+  },
+}));
+
 describe("Render BackButton component", () => {
   const user = userEvent.setup();
   beforeEach(() => {
--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 const BackButton = () => {
+  const router = useRouter();
   return (
-    <button className="back-button" onClick={() => window.history.back()}>
+    <button className="back-button" onClick={() => router.back()}>
       <span className="back-button-icon"></span>
       Go back
     </button>
