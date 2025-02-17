2eb47ff5e8aa833dbacfd5d743bfceecce3e808d
Adhisafdhol <aadheusx@gmail.com>
2025-02-18 00:52:48 +0700

    Add a test suite for CreateOnSubmit function

--- frontend/__test__/utils/form.test.tsx
+++ frontend/__test__/utils/form.test.tsx
@@ -1,4 +1,6 @@
-import { createSignupOnSubmit } from "@/utils/form";
+import { createSignupOnSubmit, createOnSubmit } from "@/utils/form";
+import { SignupForm as SignupFormType } from "../../@types/form";
+import SignupForm from "@/components/SignupForm";
 
 const errorObj = {
   message: "Failed to sign up",
@@ -99,6 +101,138 @@ describe("Test createSignupOnSubmit function", () => {
       jest.spyOn(console, "log").mockImplementation();
       await signupSubmit(mockData);
       expect(console.log).toHaveBeenCalledTimes(2);
+      expect(console.log).toHaveBeenCalledWith("Network Error");
+    });
+  });
+});
+
+describe("Test createOnSubmit function", () => {
+  const getFormDataMock = jest.fn(<SignupFormType,>(data: SignupFormType) => {
+    const formData = new URLSearchParams();
+
+    for (const prop in data) {
+      formData.append(prop, data[prop] as string);
+    }
+    return formData;
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Run errorHandler when response status >= 400", async () => {
+    const errorHandler = jest.fn(() => {});
+    const successHandler = jest.fn();
+    const getFormData = getFormDataMock;
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+    const path = "test";
+
+    const onSubmit = createOnSubmit<SignupForm>({
+      errorHandler,
+      successHandler,
+      getFormData,
+      config,
+      path,
+    });
+
+    const mockData = {
+      username: "test_user_1",
+      password: "Test_123",
+    };
+    await onSubmit(mockData);
+    expect(errorHandler).toHaveBeenCalled();
+  });
+
+  it("Run successHandler when response status < 400", async () => {
+    const errorHandler = jest.fn(() => {});
+    const successHandler = jest.fn();
+    const getFormData = getFormDataMock;
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+    const path = "test";
+
+    const onSubmit = createOnSubmit<SignupForm>({
+      errorHandler,
+      successHandler,
+      getFormData,
+      config,
+      path,
+    });
+
+    const mockData = {
+      username: "test_user_2",
+      password: "Test_123",
+    };
+    await onSubmit(mockData);
+    expect(successHandler).toHaveBeenCalled();
+  });
+
+  describe("Test createSignupOnSubmit function", () => {
+    it("Run errorHandler when response status >= 400", async () => {
+      const errorHandler = jest.fn(() => {});
+      const successHandler = jest.fn();
+      const getFormData = getFormDataMock;
+      const config = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      };
+      const path = "test";
+
+      const onSubmit = createOnSubmit<SignupFormType>({
+        errorHandler,
+        successHandler,
+        getFormData,
+        config,
+        path,
+      });
+
+      const mockData = {
+        username: "test_user_1",
+        password: "Test_123",
+      };
+      await onSubmit(mockData);
+      expect(errorHandler).toHaveBeenCalled();
+    });
+
+    it("Catch err if fetch throw an error", async () => {
+      const errorHandler = jest.fn(() => {});
+      const successHandler = jest.fn();
+      const getFormData = getFormDataMock;
+      const config = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      };
+      const path = "test";
+
+      const onSubmit = createOnSubmit<SignupFormType>({
+        errorHandler,
+        successHandler,
+        getFormData,
+        config,
+        path,
+      });
+
+      const mockData = {
+        username: "ERROR",
+        password: "Test_123",
+      };
+
+      jest.spyOn(console, "log").mockImplementation();
+      await onSubmit(mockData);
+      expect(console.log).toHaveBeenCalledTimes(1);
       expect(console.log).toHaveBeenCalledWith("Network Error");
     });
   });
