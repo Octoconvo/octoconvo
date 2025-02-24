74aa210515975e490a730ca003a652c3dd43b263
Adhisafdhol <aadheusx@gmail.com>
2025-02-24 13:21:21 +0700

    Separate prisma client instance as a singleton

--- backend/database/prisma/userQueries.ts
+++ backend/database/prisma/userQueries.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import prisma from "./client";
 
 const getUserByUsername = async (username: string) => {
   const user = await prisma.user.findFirst({
