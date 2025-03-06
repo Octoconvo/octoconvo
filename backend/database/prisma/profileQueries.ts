e76ba6569ad06739ec544e03e8275928d6530b22
Adhisafdhol <aadheusx@gmail.com>
2025-03-06 07:51:52 +0700

    feat(profile queries): add update profile query

--- backend/database/prisma/profileQueries.ts
+++ backend/database/prisma/profileQueries.ts
@@ -14,4 +14,39 @@ const getUserProfileById = async (id: string) => {
   return userProfile;
 };
 
-export { getUserProfileById };
+const updateUserProfileById = async ({
+  id,
+  displayName,
+  bio,
+  avatarURL,
+  bannerURL,
+  currentTime,
+}: {
+  id: string;
+  displayName: null | string;
+  bio: null | string;
+  avatarURL: null | string;
+  bannerURL: null | string;
+  currentTime: Date;
+}) => {
+  const userProfile = await prisma.user.update({
+    where: {
+      id: id,
+    },
+    data: {
+      ...(displayName ? { displayName: displayName } : {}),
+      ...(bio ? { bio: bio } : {}),
+      ...(avatarURL ? { avatar: avatarURL } : {}),
+      ...(bannerURL ? { banner: bannerURL } : {}),
+      updatedAt: currentTime,
+    },
+    omit: {
+      password: true,
+      isDeleted: true,
+    },
+  });
+
+  return userProfile;
+};
+
+export { getUserProfileById, updateUserProfileById };
