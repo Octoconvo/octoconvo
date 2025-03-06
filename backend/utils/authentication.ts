88ffe8897af94bf55374afb8a3b273c0ef52205b
Adhisafdhol <aadheusx@gmail.com>
2025-03-06 07:45:10 +0700

    feat(auth utils): add midleware auth handler
    Add a function to create midleware for handling unauthenticated user and
    send a response depending on the function arguments.

---
+++ backend/utils/authentication.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from "express";
+
+const createAuthenticationHandler = ({
+  message,
+  errMessage,
+}: {
+  message: string;
+  errMessage: string;
+}): RequestHandler => {
+  return (req, res, next) => {
+    const user = req.user;
+
+    if (!user) {
+      res.status(401).json({
+        message,
+        error: {
+          message: errMessage,
+        },
+      });
+    }
+
+    next();
+  };
+};
+
+export { createAuthenticationHandler };
