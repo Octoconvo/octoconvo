6fcd8c6f8b02948fb34a25152518e8a9c67e5f04
Adhisafdhol <aadheusx@gmail.com>
2025-03-12 10:11:03 +0700

    feat(lobby nav): add lobby nav component

--- frontend/src/components/LobbyNav.tsx
+++ frontend/src/components/LobbyNav.tsx
@@ -7,85 +7,93 @@ const LobbyNav = () => {
   const pathname = usePathname();
 
   return (
-    <nav
-      className={
-        "flex flex-col justify-between h-full bg-black-100 py-12" + " px-4"
-      }
-    >
-      <div className="flex flex-col justify-between gap-8">
-        <Link
-          href={`${pathname}\\friends`}
-          className={
-            "flex relative items-center justify-center h-12 transition-all" +
-            " hover:transition-all hover:shadow-[0_0.1rem_0_0_var(--white-200)]"
-          }
-        >
-          <span
+    <>
+      <nav
+        className={
+          "flex flex-col justify-between h-full bg-black-100 py-12" + " px-4"
+        }
+      >
+        <div className="flex flex-col justify-between gap-8">
+          <Link
+            data-testid="friends-l"
+            href={`${pathname}/friends`}
             className={
-              "lobby-nav-icon" + " after:bg-[url(/images/friends-icon.svg)]"
+              "flex relative items-center justify-center h-12 transition-all" +
+              " hover:transition-all hover:shadow-[0_0.1rem_0_0_var(--white-200)]"
             }
-          ></span>
-        </Link>
-        <Link
-          href={`${pathname}\\dm`}
-          className={
-            "flex relative items-center justify-center h-12 translation-all" +
-            " hover:transition-all hover:shadow-[0_0.1rem_0_0_var(--white-200)]"
-          }
-        >
-          <span
+          >
+            <span
+              className={
+                "lobby-nav-icon" + " after:bg-[url(/images/friends-icon.svg)]"
+              }
+            ></span>
+          </Link>
+          <Link
+            data-testid="dm-l"
+            href={`${pathname}/dm`}
             className={
-              "lobby-nav-icon" + " after:bg-[url(/images/dm-icon.svg)]"
+              "flex relative items-center justify-center h-12 translation-all" +
+              " hover:transition-all hover:shadow-[0_0.1rem_0_0_var(--white-200)]"
             }
-          ></span>
-        </Link>
-        <Link
-          href={`${pathname}\\communities`}
-          className={
-            "flex relative items-center justify-center h-12 translation-all" +
-            " hover:transition-all hover:shadow-[0_0.1rem_0_0_var(--white-200)]"
-          }
-        >
-          <span
+          >
+            <span
+              className={
+                "lobby-nav-icon" + " after:bg-[url(/images/dm-icon.svg)]"
+              }
+            ></span>
+          </Link>
+          <Link
+            data-testid="communities-l"
+            href={`${pathname}/communities`}
             className={
-              "lobby-nav-icon" + " after:bg-[url(/images/communities-icon.svg)]"
+              "flex relative items-center justify-center h-12 translation-all" +
+              " hover:transition-all hover:shadow-[0_0.1rem_0_0_var(--white-200)]"
             }
-          ></span>
-        </Link>
-        <Link
-          href={`${pathname}//communities//create`}
-          className={
-            "flex relative items-center justify-center h-12 before:bg-brand-1" +
-            " before:w-full before:h-full before:rounded-full transition-all" +
-            " hover:transition-all hover:before:scale-105" +
-            " hover:before:rounded-[1rem]"
-          }
-        >
-          <span
+          >
+            <span
+              className={
+                "lobby-nav-icon" +
+                " after:bg-[url(/images/communities-icon.svg)]"
+              }
+            ></span>
+          </Link>
+          <Link
+            data-testid="create-community-l"
+            href={`${pathname}/community/create`}
             className={
-              "lobby-nav-icon" + " after:bg-[url(/images/add-icon.svg)]"
+              "flex relative items-center justify-center h-12 before:bg-brand-1" +
+              " before:w-full before:h-full before:rounded-full transition-all" +
+              " hover:transition-all hover:before:scale-105" +
+              " hover:before:rounded-[1rem]"
             }
-          ></span>
-        </Link>
-        <Link
-          href={`${pathname}\\explore`}
-          className={
-            "flex relative items-center justify-center h-12 before:bg-brand-1" +
-            " before:rounded-full transition-all hover:transition-all" +
-            " hover:before:scale-105 before:w-full before:h-full" +
-            " hover:before:rounded-[1rem]"
-          }
-        >
-          <span
+          >
+            <span
+              className={
+                "lobby-nav-icon" + " after:bg-[url(/images/add-icon.svg)]"
+              }
+            ></span>
+          </Link>
+          <Link
+            data-testid="explore-l"
+            href={`${pathname}/explore`}
             className={
-              "lobby-nav-icon after:bg-[url(/images/explore-icon.svg)]"
+              "flex relative items-center justify-center h-12 before:bg-brand-1" +
+              " before:rounded-full transition-all hover:transition-all" +
+              " hover:before:scale-105 before:w-full before:h-full" +
+              " hover:before:rounded-[1rem]"
             }
-          ></span>
-        </Link>
-      </div>
+          >
+            <span
+              className={
+                "lobby-nav-icon after:bg-[url(/images/explore-icon.svg)]"
+              }
+            ></span>
+          </Link>
+        </div>
 
-      <button className="rounded-full h-[3rem] w-[3rem] bg-grey-200"></button>
-    </nav>
+        <button className="rounded-full h-[3rem] w-[3rem] bg-grey-200"></button>
+      </nav>
+    </>
   );
 };
 
