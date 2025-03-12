2f078c0a843504dd124a68dab30cc04de6a0289f
Adhisafdhol <aadheusx@gmail.com>
2025-03-13 03:42:46 +0700

    test(date utils): test date string return value

---
+++ frontend/__test__/utils/date.test.tsx
@@ -0,0 +1,10 @@
+import { formatDateString } from "@/utils/date";
+
+describe("Test transformDateString function", () => {
+  test("return correct date string in january 01 2000 format", () => {
+    const date = formatDateString("2025-03-12T20:09:55.245Z");
+    const dateChecker = new Date("2025-03-12T20:09:55.245Z");
+
+    expect(date).toBe("March " + `${dateChecker.getDate()}` + " 2025");
+  });
+});
