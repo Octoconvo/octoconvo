ea8cc44bc16625269b9ade9bd5c4cad396f8c2c6
Adhisafdhol <aadheusx@gmail.com>
2025-03-22 16:27:50 +0700

    fix(app layout): update UserProfileProvider import

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./global.css";
 import { inter } from "@/fonts";
 import SocketWrapper from "@/components/SocketWrapper";
 import UserProvider from "@/components/UserProvider";
+import UserProfileProvider from "@/components/UserProfileProvider";
 
 export const metadata: Metadata = {
   title: "Octoconvo",
@@ -18,7 +19,9 @@ export default function RootLayout({
     <html lang="en" className="min-h-[100dvh]">
       <body className={`${inter.variable} font-sans flex h-full`}>
         <SocketWrapper>
-          <UserProvider>{children}</UserProvider>
+          <UserProvider>
+            <UserProfileProvider>{children}</UserProfileProvider>
+          </UserProvider>
         </SocketWrapper>
       </body>
     </html>
