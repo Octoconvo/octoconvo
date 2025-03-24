6a766e81d8efa868b85619b7d1d2b5dd20173dfa
Adhisafdhol <aadheusx@gmail.com>
2025-03-24 17:13:50 +0700

    test(authentication): add test suite for logout

--- frontend/__test__/utils/authentication.test.tsx
+++ frontend/__test__/utils/authentication.test.tsx
@@ -1,4 +1,4 @@
-import { checkAuthStatus } from "@/utils/authentication";
+import { checkAuthStatus, logout } from "@/utils/authentication";
 
 const successObj = {
   message: "You are authenticated",
@@ -6,18 +6,81 @@ const successObj = {
     id: 12345678,
   },
 };
+
+const successObjLogout = {
+  message: "Successfully logout",
+};
+
+const errObjLogout = {
+  message: "Failed to logout",
+};
+
 type Config = { body: URLSearchParams };
-// eslint-disable-next-line
-global.fetch = jest.fn((_url, config: Config) => {
-  return Promise.resolve({
-    status: 200,
-    json: () => Promise.resolve(successObj),
-  });
-}) as jest.Mock;
+
+global.fetch = jest
+  .fn()
+  .mockImplementationOnce(
+    // eslint-disable-next-line
+    jest.fn((_url, config: Config) => {
+      return Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(successObj),
+      });
+    })
+  )
+  .mockImplementationOnce(
+    // eslint-disable-next-line
+    jest.fn((_url, config: Config) => {
+      return Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(successObjLogout),
+      });
+    })
+  )
+  .mockImplementationOnce(
+    // eslint-disable-next-line
+    jest.fn((_url, config: Config) => {
+      return Promise.resolve({
+        status: 400,
+        json: () => Promise.resolve(errObjLogout),
+      });
+    })
+  )
+  .mockImplementationOnce(
+    // eslint-disable-next-line
+    jest.fn((_url, config: Config) => {
+      return Promise.resolve().then(() => {
+        throw new Error("Network Error");
+      });
+    })
+  ) as jest.Mock;
 
 describe("Test checkAuthStatus function", () => {
   test("Return user obj if user is logged in", async () => {
     const user = await checkAuthStatus();
     expect(user).toBe(successObj.user);
+  });
+});
+
+describe("Test logout function", () => {
+  test("Run successHandler when fetch is successful", async () => {
+    const successHandler = jest.fn();
+    const errorHandler = jest.fn();
+    await logout({ successHandler, errorHandler });
+    expect(successHandler).toHaveBeenCalled();
+  });
+
+  test("Run errorHandler when response status >= 400", async () => {
+    const successHandler = jest.fn();
+    const errorHandler = jest.fn();
+    await logout({ successHandler, errorHandler });
+    expect(errorHandler).toHaveBeenCalled();
+  });
+
+  test("Run errorHandler on caught error", async () => {
+    const successHandler = jest.fn();
+    const errorHandler = jest.fn();
+    await logout({ successHandler, errorHandler });
+    expect(errorHandler).toHaveBeenCalled();
   });
 });
