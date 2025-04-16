6debf58193a80c897b5f1979cc5d9884bbfaaa27
Adhisafdhol <aadheusx@gmail.com>
2025-04-16 07:12:32 +0700

    feat(layout): add EditProfileModalProvider
    Add EditProfileModalProvider to lobby.

--- frontend/src/app/lobby/layout.tsx
+++ frontend/src/app/lobby/layout.tsx
@@ -1,6 +1,7 @@
 import ActiveModalProvider from "@/components/ActiveModalProvider";
 import LobbyNavWrapper from "@/components/LobbyNavWrapper";
 import ProtectedRouteWrapper from "@/components/ProtectedRouteWrapper";
+import EditProfileModalProvider from "@/components/EditProfileModalProvider";
 
 export default async function Layout({
   children,
@@ -8,11 +9,13 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-auto bg-black-200">
+    <div className="relative flex flex-auto bg-black-200">
       <ProtectedRouteWrapper route="\login">
         <ActiveModalProvider>
-          <LobbyNavWrapper />
-          {children}
+          <EditProfileModalProvider>
+            <LobbyNavWrapper />
+            {children}
+          </EditProfileModalProvider>
         </ActiveModalProvider>
       </ProtectedRouteWrapper>
     </div>
