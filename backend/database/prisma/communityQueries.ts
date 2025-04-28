c2f36be8a9f82611a67fd5986063693ed5561beb
Adhisafdhol <aadheusx@gmail.com>
2025-04-28 10:20:42 +0700

    feat(database): add prisma community queries
    - Add deleteCommunityById
    - Add getCommunityByName
    - Add createCommunity.

---
+++ backend/database/prisma/communityQueries.ts
@@ -0,0 +1,75 @@
+import prisma from "./client";
+
+const deleteCommunityById = async (id: string) => {
+  const deleteCommunity = prisma.community.delete({
+    where: {
+      id,
+    },
+  });
+
+  const deleteInbox = prisma.inbox.delete({
+    where: {
+      communityId: id,
+    },
+  });
+
+  const deleteParticipants = prisma.participant.deleteMany({
+    where: {
+      communityId: id,
+    },
+  });
+
+  await prisma.$transaction([deleteInbox, deleteParticipants, deleteCommunity]);
+};
+
+const getCommunityByName = async (name: string) => {
+  const community = await prisma.community.findFirst({
+    where: {
+      name: {
+        equals: name,
+        mode: "insensitive",
+      },
+    },
+  });
+
+  return community;
+};
+
+const createCommunity = async ({
+  id,
+  name,
+  bio,
+  avatar,
+  banner,
+}: {
+  id: string;
+  name: string;
+  bio: string | null;
+  avatar: string | null;
+  banner: string | null;
+}) => {
+  const community = await prisma.community.create({
+    data: {
+      name,
+      bio,
+      avatar,
+      banner,
+      inbox: {
+        create: {
+          inboxType: "COMMUNITY",
+        },
+      },
+      participants: {
+        create: {
+          userId: id,
+          role: "OWNER",
+          status: "ACTIVE",
+        },
+      },
+    },
+  });
+
+  return community;
+};
+
+export { getCommunityByName, createCommunity, deleteCommunityById };
