66702cfe02569c20d4ea681683fcf1e115e47579
Adhisafdhol <aadheusx@gmail.com>
2025-05-02 12:47:47 +0700

    test(components): add CreateCommunityModal tests

---
+++ frontend/__tests__/components/CreateCommunity/CreateCommunityModal.test.tsx
@@ -0,0 +1,71 @@
+import CreateCommunityModal from "@/components/CreateCommunity/CreateCommunityModal";
+import { act, render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { ActiveModalsContext } from "@/contexts/modal";
+import ActiveModalProvider from "@/components/ActiveModalProvider";
+import CreateCommunityModalProvider from "@/components/CreateCommunity/CreateCommunityModalProvider";
+import LobbyNavWrapper from "@/components/LobbyNavWrapper";
+
+const openModalMock = jest.fn();
+const closeModalMock = jest.fn();
+
+describe("Render CreateCommunityModal", () => {
+  const user = userEvent.setup();
+
+  beforeEach(() => {
+    render(
+      <ActiveModalsContext.Provider
+        value={{
+          activeModals: [],
+          openModal: openModalMock,
+          closeModal: closeModalMock,
+        }}
+      >
+        <CreateCommunityModal></CreateCommunityModal>
+      </ActiveModalsContext.Provider>
+    );
+  });
+
+  test("Close button work correctly", async () => {
+    const button = screen.getByTestId("crt-cmmnty-mdl-cls-btn");
+
+    expect(button).toBeDefined();
+
+    await user.click(button);
+
+    expect(closeModalMock).toHaveBeenCalled();
+  });
+});
+
+describe("Render CreateCommunityModal", () => {
+  const user = userEvent.setup();
+
+  beforeEach(async () => {
+    await act(async () => {
+      render(
+        <ActiveModalProvider>
+          <CreateCommunityModalProvider>
+            <LobbyNavWrapper></LobbyNavWrapper>
+          </CreateCommunityModalProvider>
+        </ActiveModalProvider>
+      );
+    });
+  });
+
+  test("Show modal when ActiveModals contain CreateCommunityModalProvider", async () => {
+    const modal = screen.getByTestId("crt-cmmnty-modal-main");
+    const openBtn = screen.getByTestId("crt-cmmnty-mdl-opn-btn");
+    const closeBtn = screen.getByTestId("crt-cmmnty-mdl-cls-btn");
+
+    expect(modal).toBeDefined();
+    expect(openBtn).toBeDefined();
+
+    await user.click(openBtn);
+
+    expect(modal.className).not.toContain("hidden");
+
+    await user.click(closeBtn);
+
+    expect(modal.className).toContain("hidden");
+  });
+});
