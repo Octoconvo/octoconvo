68e18622cddbaeb86852f5d4d481f8a884fce275
Adhisafdhol <aadheusx@gmail.com>
2025-05-02 11:36:01 +0700

    feat(types): add community POST response type

---
+++ frontend/src/types/response.ts
@@ -0,0 +1,12 @@
+type CommunityResponsePOST = {
+  id: string;
+  name: string;
+  bio: null | string;
+  avatar: null | string;
+  banner: null | string;
+  isDeleted: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type { CommunityResponsePOST };
