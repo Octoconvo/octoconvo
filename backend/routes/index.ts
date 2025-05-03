ed9a04845f64f51fdcb6bca7553e4223ff57dd17
Adhisafdhol <aadheusx@gmail.com>
2025-05-03 11:21:38 +0700

    feat(routes): add communities get route

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import express from "express";
+import * as communityController from "../controllers/community";
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req: Request, res: Response) {
   res.send("index");
 });
+
+router.get("/communities", communityController.communities_get);
 
 export default router;
