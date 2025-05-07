21b7296d79eafce72da2fe3671523d89747f6eed
Adhisafdhol <aadheusx@gmail.com>
2025-05-07 20:45:09 +0700

    test(components): add CommunitiesItem test suites

---
+++ frontend/__tests__/components/CommunitiesList/CommunitiesList.test.tsx
@@ -0,0 +1,74 @@
+import CommunitiesItem from "@/components/Communities/CommunitiesList/CommunitiesItem";
+import { render, act, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { userEvent } from "@testing-library/user-event";
+
+const communitiesList = [
+  {
+    id: "1",
+    name: "Community_Test_Name",
+    bio: "Community_Test_Bio",
+    avatar: "http://Community_Test_Avatar",
+    banner: null,
+    isDeleted: false,
+    createdAt: "test",
+    updatedAt: "test",
+  },
+  {
+    id: "2",
+    name: "Community_Test_Name_1",
+    bio: "Community_Test_Bio_1",
+    avatar: null,
+    banner: null,
+    isDeleted: false,
+    createdAt: "test",
+    updatedAt: "test",
+  },
+];
+
+describe("Render communitiesList one", () => {
+  const user = userEvent.setup();
+
+  beforeEach(async () => {
+    await act(async () =>
+      render(<CommunitiesItem community={communitiesList[0]} />)
+    );
+  });
+
+  test("Render community name", () => {
+    const name = screen.getByText(communitiesList[0].name);
+
+    expect(name).toBeInTheDocument();
+  });
+
+  test("Render avatar when it's not null", () => {
+    const avatar = screen.getByAltText(communitiesList[0].name + " avatar");
+
+    expect(avatar).toBeInTheDocument();
+  });
+
+  test("Test hover style change on community list item", async () => {
+    const li = screen.getByTestId("cmmnts-cmmnty-lst");
+    const div = screen.getByTestId("cmmnts-cmmnty-lst-avatar-cntnr");
+    expect(div.classList).toContain("bg-gr-main-r");
+
+    await user.hover(li);
+    expect(div.classList).toContain("bg-black-100");
+    expect(div.classList).not.toContain("bg-gr-main-r");
+    await user.unhover(li);
+    expect(div.classList).not.toContain("bg-black-100");
+    expect(div.classList).toContain("bg-gr-main-r");
+  });
+});
+
+describe("Render communitiesList two", () => {
+  beforeEach(async () => {
+    render(<CommunitiesItem community={communitiesList[1]} />);
+  });
+
+  test("Don't render avatar image when it's null", () => {
+    const avatar = screen.queryByAltText(communitiesList[1].name + " avatar");
+
+    expect(avatar).not.toBeInTheDocument();
+  });
+});
