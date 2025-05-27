9f5e3e18ad8af684abd5c730e43c1be6be8ca2f0
Adhisafdhol <aadheusx@gmail.com>
2025-05-28 01:10:17 +0700

    feat(utils): add a function to validate UUID

--- backend/utils/validation.ts
+++ backend/utils/validation.ts
@@ -1,3 +1,11 @@
+const isUUID = (value: string): boolean => {
+  const regex = new RegExp(
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/,
+  );
+
+  return regex.test(value);
+};
+
 const isISOString = (value: string) => {
   try {
     const date = new Date(value);
@@ -11,4 +19,4 @@ const isISOString = (value: string) => {
   }
 };
 
-export { isISOString };
+export { isUUID, isISOString };
