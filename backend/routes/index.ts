27353a2d5fad26d038147fdabb5c766312e62b97
Adhisafdhol <aadheusx@gmail.com>
2025-05-28 04:12:20 +0700

    feat(routes): add messages GET route

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import express from "express";
 import * as communityController from "../controllers/community";
+import * as messageController from "../controllers/message";
 const router = express.Router();
 
 /* GET home page. */
@@ -9,5 +10,7 @@ router.get("/", function (req: Request, res: Response) {
 });
 
 router.get("/communities", communityController.communities_get);
+
+router.get("/messages", messageController.messages_get);
 
 export default router;
