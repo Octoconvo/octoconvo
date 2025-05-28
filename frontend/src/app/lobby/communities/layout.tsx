d984d8944e3db3698c0822001f1c51b423fc2ed8
Adhisafdhol <aadheusx@gmail.com>
2025-05-29 01:11:25 +0700

    fix(UI): fix community background style
    - Fix communities layout to show the dynamic community routes on top of
    the community container background.
    - Remove unused style.

--- frontend/src/app/lobby/communities/layout.tsx
+++ frontend/src/app/lobby/communities/layout.tsx
@@ -1,20 +1,10 @@
 import CommunitiesListWrapper from "@/components/Communities/CommunitiesList/CommunitiesListWrapper";
 
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-h-[100dvh] flex flex-auto bg-gradient-to-r from-brand-2 from-30% to-brand-1 to-100% animate-[300ms_fade-out]">
       <CommunitiesListWrapper />
-      <div className="relative flex items-center justify-center w-full ">
-        <div className="community-circle">
-          <div className="flex justify-center items-center rounded-full h-[256px] w-[256px]">
-            <span className="octoconvo-community-icon after:w-[128px] after:h-[128px]"></span>
-          </div>
-        </div>
-
+      <div className="community-bg relative flex items-center justify-center w-full ">
         {children}
       </div>
     </div>
