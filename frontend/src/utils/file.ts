8bd85bdc2d4645fc6269a19542189bda217aa4e0
Adhisafdhol <aadheusx@gmail.com>
2025-06-03 23:06:36 +0700

    feat(utils): add function to validate input files

--- frontend/src/utils/file.ts
+++ frontend/src/utils/file.ts
@@ -13,11 +13,7 @@ const readFileAsDataURL = async ({ file }: { file: File }): Promise<string> => {
   });
 };
 
-const previewFile = async ({
-  file,
-}: {
-  file: File;
-}): Promise<string | null> => {
+const previewFile = async ({ file }: { file: File }): Promise<string | null> => {
   try {
     const image: string = await readFileAsDataURL({ file });
     return image;
@@ -50,4 +46,42 @@ const selectFile = (
   }
 };
 
-export { readFileAsDataURL, previewFile, selectFile };
+// Validate files
+const validateFiles = ({
+  e,
+  mimetype,
+  maxSize,
+}: {
+  e: React.FormEvent<HTMLInputElement>;
+  mimetype: RegExp | string[];
+  maxSize: number;
+}): FileList | null => {
+  const fileInput = e.target as HTMLInputElement;
+  const files = fileInput.files ? fileInput.files : null;
+
+  if (files) {
+    for (const file of files) {
+      if (mimetype instanceof RegExp) {
+        const isValid = mimetype.test(file.type);
+
+        if (!isValid) {
+          throw new Error("Invalid mimetype");
+        }
+      } else {
+        const isValid = mimetype.includes(file.type);
+
+        if (!isValid) {
+          throw new Error("Invalid mimetype");
+        }
+      }
+
+      if (file.size > maxSize) {
+        throw new Error("File size is too big");
+      }
+    }
+  }
+
+  return files;
+};
+
+export { readFileAsDataURL, previewFile, selectFile, validateFiles };
