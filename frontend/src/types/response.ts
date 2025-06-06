2af86126d27b179a1413a7b47dfe56b66a7e5edc
Adhisafdhol <aadheusx@gmail.com>
2025-06-06 11:34:31 +0700

    feat(types): add inbox in communitesResponseGET

--- frontend/src/types/response.ts
+++ frontend/src/types/response.ts
@@ -18,6 +18,12 @@ type CommunitiesResponseGET = {
   isDeleted: boolean;
   createdAt: string;
   updatedAt: string;
+  inbox: {
+    id: string;
+    inboxType: "COMMUNITY" | "DM";
+    communityId: null | string;
+    directMessageId: null | string;
+  };
 };
 
 export type { CommunityResponsePOST, CommunitiesResponseGET };
