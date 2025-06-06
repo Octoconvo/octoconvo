ad4dec7367fbc3ca77d1a5dbe69a349ced10eb8f
Adhisafdhol <aadheusx@gmail.com>
2025-06-06 12:31:58 +0700

    feat(components): add CommunitiesItem

--- frontend/src/components/Communities/CommunitiesList/CommunitiesItem.tsx
+++ frontend/src/components/Communities/CommunitiesList/CommunitiesItem.tsx
@@ -2,14 +2,12 @@
 
 import Image from "next/image";
 import { CommunitiesResponseGET } from "@/types/response";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import Link from "next/link";
 
-const CommunitiesItem = ({
-  community,
-}: {
-  community: CommunitiesResponseGET;
-}) => {
+const CommunitiesItem = ({ community }: { community: CommunitiesResponseGET }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const linkRef = useRef<null | HTMLAnchorElement>(null);
 
   return (
     <li
@@ -20,19 +18,26 @@ const CommunitiesItem = ({
       onMouseLeave={() => {
         setIsHovered(false);
       }}
-      className="flex w-full border-b-[1px] border-[#80fffb1a]"
+      className="flex box-border w-full max-w-full border-b-[1px] border-[#80fffb1a] overflow-hidden"
     >
-      <button className="flex w-full items-center gap-4 p-8 font-semibold text-h6 hover:bg-brand-1 hover:rounded-[8px]">
+      <button
+        onClick={() => {
+          if (linkRef.current) {
+            linkRef.current.click();
+          }
+        }}
+        className="flex w-full box-border items-center gap-4 p-8 font-semibold text-h6 hover:bg-brand-1 hover:rounded-[8px]"
+      >
         <div
           data-testid="cmmnts-cmmnty-lst-avatar-cntnr"
           className={
-            "w-12 h-12  rounded-tr-[8px] rounded-br-[8px]" +
+            "w-12 h-12 min-w-[64px] min-h-[64px] rounded-tr-[8px] rounded-br-[8px]" +
             (isHovered ? " bg-black-100" : " bg-gr-main-r")
           }
         >
           {community.avatar && (
             <Image
-              className="rounded-[inherit]"
+              className="rounded-[inherit] min-w-[64px] min-h-[64px]"
               src={community.avatar}
               alt={`${community.name} avatar`}
               width={64}
@@ -40,8 +45,13 @@ const CommunitiesItem = ({
             />
           )}
         </div>
-
-        {community.name}
+        <Link
+          className="box-border max-w-full overflow-hidden text-ellipsis"
+          ref={linkRef}
+          href={`/lobby/communities/${community.id}`}
+        >
+          {community.name}
+        </Link>
       </button>
     </li>
   );
