bf3433753e877b6a4cf40cf5453d4e21b45b8b92
Adhisafdhol <aadheusx@gmail.com>
2025-06-07 00:22:41 +0700

    fix(layout): fix communities layout max width
    Change communities layout to not exceed its parent's full width.

--- frontend/src/app/lobby/communities/layout.tsx
+++ frontend/src/app/lobby/communities/layout.tsx
@@ -1,10 +1,20 @@
 import CommunitiesListWrapper from "@/components/Communities/CommunitiesList/CommunitiesListWrapper";
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <div className="max-h-[100dvh] flex flex-auto bg-gradient-to-r from-brand-2 from-30% to-brand-1 to-100% animate-[300ms_fade-out]">
+    <div
+      className={
+        "grid grid-cols-[minmax(400px,1fr)_minmax(0,2.83fr)] w-full" +
+        " bg-gradient-to-r from-brand-2 from-30% to-brand-1 to-100%" +
+        " animate-[300ms_fade-out]"
+      }
+    >
       <CommunitiesListWrapper />
-      <div className="community-bg relative flex items-center justify-center w-full ">
+      <div className="community-bg flex items-center justify-center w-full ">
         {children}
       </div>
     </div>
