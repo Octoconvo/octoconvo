8c1f0c29412c2809233c4ba5dd7d18c50b281e8b
Adhisafdhol <aadheusx@gmail.com>
2025-06-22 16:05:43 +0700

    feat(database): add a query to explore communities

--- backend/database/prisma/communityQueries.ts
+++ backend/database/prisma/communityQueries.ts
@@ -1,3 +1,4 @@
+import { Community } from "@prisma/client";
 import prisma from "./client";
 
 const deleteCommunityById = async (id: string) => {
@@ -127,6 +128,80 @@ const getCommunityByIdAndParticipant = async ({
   return community;
 };
 
+const searchCommunities = async ({
+  name,
+  cursor,
+  limit,
+}: {
+  name: string | null;
+  limit: number;
+  cursor: null | {
+    memberCount: number;
+    id: string;
+    createdAt: string;
+  };
+}) => {
+  const nameQuery: string = name ? `%${name}%` : "";
+  const cursorQuery = cursor ? "TRUE" : "FALSE";
+  const memberCountQuery = cursor ? cursor.memberCount : 0;
+  const createdAtQuerry = cursor ? new Date(cursor.createdAt) : new Date();
+  const idQuery = cursor ? cursor.id : "";
+
+  type CommunityDataRaw = Community & {
+    participants?: number;
+  };
+
+  const communities: CommunityDataRaw[] = await prisma.$queryRaw`
+    SELECT * FROM (
+      SELECT *, 
+      CAST((
+        SELECT COUNT(*) FROM "Participant" 
+        WHERE "Participant"."communityId" = "Community"."id"
+        ) AS INT
+      ) AS participants
+    FROM "Community" 
+    WHERE
+      (CASE WHEN ${nameQuery} = '' THEN "Community"."id" = "Community"."id"
+      ELSE UPPER("Community"."name") LIKE UPPER(${nameQuery})
+      END) 
+    ORDER BY 
+      participants DESC
+      , "Community"."createdAt" ASC
+      , "Community"."id" DESC
+    )
+    WHERE 
+      (CASE WHEN ${cursorQuery} = 'FALSE' THEN "id" = "id"
+      ELSE (
+      "participants" <= CAST(${memberCountQuery} AS INT) 
+      AND "createdAt" >= CAST(${createdAtQuerry} AS Date) 
+      AND NOT "id" = ${idQuery})
+      END) 
+    LIMIT ${limit}`;
+
+  type CommunityData = Community & {
+    _count: {
+      participants: number;
+    };
+  };
+
+  const updateCount = communities.map(community => {
+    return new Promise<CommunityData>(resolve => {
+      const updatedCommunity = {
+        ...community,
+        _count: {
+          participants: community.participants as number,
+        },
+      };
+      delete updatedCommunity["participants"];
+      resolve(updatedCommunity);
+    });
+  });
+
+  const data = await Promise.all(updateCount);
+
+  return data;
+};
+
 export {
   getCommunityById,
   getCommunityByName,
@@ -134,4 +209,5 @@ export {
   deleteCommunityById,
   getUserCommunities,
   getCommunityByIdAndParticipant,
+  searchCommunities,
 };
