f82e125b9d79850d733c696e3fa5ee905ee66f85
Adhisafdhol <aadheusx@gmail.com>
2025-06-30 21:49:17 +0700

    test(components): add CommunityItem's tests

---
+++ frontend/__tests__/components/Explore/CommunityItem.test.tsx
@@ -0,0 +1,50 @@
+import CommunityItem from "@/components/Explore/CommunityItem";
+import userEvent from "@testing-library/user-event";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { CommunityExploreGET } from "@/types/response";
+
+const community1: CommunityExploreGET = {
+  id: "testid1",
+  _count: {
+    participants: 1,
+  },
+  name: "testname1",
+  bio: "tastbio1",
+  avatar: "testavatar1",
+  banner: "testbanner1",
+  isDeleted: false,
+  createdAt: "testcreatedat1",
+  updatedAt: "testupdatedat1",
+};
+
+describe("Render CommunityItem", () => {
+  const user = userEvent.setup();
+
+  test("Render 'member' when the participants' count is one", () => {
+    render(<CommunityItem community={community1} />);
+
+    const participantInfo = screen.getByTestId(
+      "cmmnty-itm-prtcpnts"
+    ) as HTMLParagraphElement;
+    expect(participantInfo.textContent).toBe("1 member");
+  });
+
+  test("Render 'members' when the participants' count is not one", () => {
+    render(
+      <CommunityItem
+        community={{
+          ...community1,
+          _count: {
+            participants: 2,
+          },
+        }}
+      />
+    );
+
+    const participantInfo = screen.getByTestId(
+      "cmmnty-itm-prtcpnts"
+    ) as HTMLParagraphElement;
+    expect(participantInfo.textContent).toBe("2 members");
+  });
+});
