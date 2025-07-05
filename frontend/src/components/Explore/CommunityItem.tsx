f284339be941f2a7b1568e33b6846dcc7a90cb35
Adhisafdhol <aadheusx@gmail.com>
2025-07-05 18:26:12 +0700

    fix(UI): fix CommunityItem banner img border
    Fix banner img element border radius on Explore Page CommunityItem.

--- frontend/src/components/Explore/CommunityItem.tsx
+++ frontend/src/components/Explore/CommunityItem.tsx
@@ -4,16 +4,25 @@ import { unescapeString } from "@/utils/string";
 const CommunityItem = ({ community }: { community: CommunityExploreGET }) => {
   return (
     <li className="flex flex-col bg-black-100 h-[300px] rounded-[8px]">
-      <figure className="bg-gr-black-2-r aspect-[3/1] rounded-[8px] rounded-b-none">
+      <figure
+        className={
+          "bg-gr-black-2-r aspect-[3/1] rounded-[8px]" + " rounded-b-none"
+        }
+      >
         {community.banner && (
           <img
-            className="object-center object-cover aspect-[3/1]"
+            className="object-center object-cover aspect-[3/1] rounded-[inherit]"
             src={community.banner}
           />
         )}
       </figure>
       <div className="relative p-[32px] pt-[36px] flex-auto">
-        <figure className="top-[-28px] absolute rounded-[8px] p-[8px] bg-black-100 w-[min-content]">
+        <figure
+          className={
+            "top-[-28px] absolute rounded-[8px] p-[8px]" +
+            " bg-black-100 w-[min-content]"
+          }
+        >
           <div className="rounded-[8px] bg-white-100 w-[48px] h-[48px]">
             {community.avatar && (
               <img src={community.avatar} className="rounded-[inherit]" />
@@ -22,10 +31,20 @@ const CommunityItem = ({ community }: { community: CommunityExploreGET }) => {
         </figure>
         <article className="flex flex-col justify-between h-full">
           <div className="overflow-hidden]">
-            <h1 className="text-p font-bold text-white-100 overflow-hidden text-ellipsis text-nowrap">
+            <h1
+              className={
+                "text-p font-bold text-white-100 overflow-hidden" +
+                " text-ellipsis text-nowrap"
+              }
+            >
               {unescapeString(community.name)}
             </h1>
-            <p className="text-p text-white-200 overflow-hidden text-ellipsis text-nowrap">
+            <p
+              className={
+                "text-p text-white-200 overflow-hidden text-ellipsis" +
+                " text-nowrap"
+              }
+            >
               {community.bio && unescapeString(community.bio)}
             </p>
           </div>
