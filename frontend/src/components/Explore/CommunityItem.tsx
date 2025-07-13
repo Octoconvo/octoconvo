9e5a4eadee95a7e5d19166706551e846d848268a
Adhisafdhol <aadheusx@gmail.com>
2025-07-13 23:09:08 +0700

    feat: implement button onclick fn on CommunityItem
    Add and implement a button on CommunityItem component for the Explore
    page to set the active community. This function will set the active
    community to be zoomed in and viewed in a modal.

--- frontend/src/components/Explore/CommunityItem.tsx
+++ frontend/src/components/Explore/CommunityItem.tsx
@@ -1,63 +1,53 @@
 import { CommunityExploreGET } from "@/types/response";
 import { unescapeString } from "@/utils/string";
+import { ActiveExploreCommunity } from "@/contexts/community";
+import { useContext } from "react";
 
 const CommunityItem = ({ community }: { community: CommunityExploreGET }) => {
+  const { setActiveCommunity } = useContext(ActiveExploreCommunity);
+
   return (
     <li className="flex flex-col bg-black-100 h-[300px] rounded-[8px]">
-      <figure
-        className={
-          "bg-gr-black-2-r aspect-[3/1] rounded-[8px]" + " rounded-b-none"
-        }
+      <button
+        data-testid="xplr-cmmnty-itm-btn"
+        onClick={() => setActiveCommunity(community)}
       >
-        {community.banner && (
-          <img
-            className="object-center object-cover aspect-[3/1] rounded-[inherit]"
-            src={community.banner}
-          />
-        )}
-      </figure>
-      <div className="relative p-[32px] pt-[36px] flex-auto">
-        <figure
-          className={
-            "top-[-28px] absolute rounded-[8px] p-[8px]" +
-            " bg-black-100 w-[min-content]"
-          }
-        >
-          <div className="rounded-[8px] bg-white-100 w-[48px] h-[48px]">
-            {community.avatar && (
-              <img src={community.avatar} className="rounded-[inherit]" />
-            )}
-          </div>
+        <figure className="bg-gr-black-2-r aspect-[3/1] rounded-[8px] rounded-b-none">
+          {community.banner && (
+            <img
+              className="object-center object-cover aspect-[3/1]"
+              src={community.banner}
+            />
+          )}
         </figure>
-        <article className="flex flex-col justify-between h-full">
-          <div className="overflow-hidden]">
-            <h1
-              className={
-                "text-p font-bold text-white-100 overflow-hidden" +
-                " text-ellipsis text-nowrap"
-              }
-            >
-              {unescapeString(community.name)}
-            </h1>
+        <div className="relative p-[32px] pt-[36px] flex-auto">
+          <figure className="top-[-28px] absolute rounded-[8px] p-[8px] bg-black-100 w-[min-content]">
+            <div className="rounded-[8px] bg-white-100 w-[48px] h-[48px]">
+              {community.avatar && (
+                <img src={community.avatar} className="rounded-[inherit]" />
+              )}
+            </div>
+          </figure>
+          <article className="flex flex-col justify-between h-full">
+            <div className="overflow-hidden">
+              <h1 className="text-start text-p font-bold text-white-100 overflow-hidden text-ellipsis text-nowrap">
+                {unescapeString(community.name)}
+              </h1>
+              <p className="text-start text-p text-white-200 overflow-hidden text-ellipsis text-nowrap">
+                {community.bio && unescapeString(community.bio)}
+              </p>
+            </div>
             <p
-              className={
-                "text-p text-white-200 overflow-hidden text-ellipsis" +
-                " text-nowrap"
-              }
+              data-testid="cmmnty-itm-prtcpnts"
+              className="text-start text-s text-white-200"
             >
-              {community.bio && unescapeString(community.bio)}
+              {community._count.participants +
+                " " +
+                (community._count.participants === 1 ? "member" : "members")}
             </p>
-          </div>
-          <p
-            data-testid="cmmnty-itm-prtcpnts"
-            className="text-s text-white-200"
-          >
-            {community._count.participants +
-              " " +
-              (community._count.participants === 1 ? "member" : "members")}
-          </p>
-        </article>
-      </div>
+          </article>
+        </div>
+      </button>
     </li>
   );
 };
