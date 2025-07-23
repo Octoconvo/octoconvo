a1cefef7f0aec87073a3fb075d6ba96f34b31345
Adhisafdhol <aadheusx@gmail.com>
2025-07-24 04:53:52 +0700

    feat(queries): add createParticipantTransaction
    Add a query to create a transaction to create a participant.

--- backend/database/prisma/participantQueries.ts
+++ backend/database/prisma/participantQueries.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import prisma from "./client";
 
 const getCommunityParticipant = async ({
@@ -28,4 +29,44 @@ const getCommunityOwner = async ({ communityId }: { communityId: string }) => {
   return participant;
 };
 
-export { getCommunityParticipant, getCommunityOwner };
+const createParticipantTransaction = async ({
+  tx,
+  userId,
+  status,
+  role,
+  type,
+  communityId,
+  directMessageId,
+}: {
+  tx: Prisma.TransactionClient;
+  userId: string;
+  status: "ACTIVE" | "PENDING";
+  role: "MEMBER" | "OWNER";
+  type: "COMMUNITY" | "DM";
+  communityId: string | null;
+  directMessageId: string | null;
+}) => {
+  return tx.participant.create({
+    data: {
+      ...(type === "COMMUNITY" && communityId
+        ? {
+            communityId: communityId,
+          }
+        : {}),
+      ...(type === "DM" && directMessageId
+        ? {
+            directMessageId: directMessageId,
+          }
+        : {}),
+      role: role,
+      userId: userId,
+      status: status,
+    },
+  });
+};
+
+export {
+  getCommunityParticipant,
+  createParticipantTransaction,
+  getCommunityOwner,
+};
