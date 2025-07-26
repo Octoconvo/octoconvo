68d469e61419c4cbce8320aef0f6d9f65d91f9d3
Adhisafdhol <aadheusx@gmail.com>
2025-07-26 13:36:38 +0700

    feat(components): add NotificationProvider
    Add a component to handle user's notification context.

---
+++ frontend/src/components/NotificationProvider.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "@/contexts/user";
+import { NotificationCountContext } from "@/contexts/notification";
+import socket from "@/socket/socket";
+import { connectToRoom } from "@/socket/eventHandler";
+
+const NotificationCountProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const { user } = useContext(UserContext);
+  const [notificationCount, setNotificationCount] = useState<null | number>(
+    null
+  );
+
+  useEffect(() => {
+    const fetchNotificationCount = async (id: string) => {
+      const domainURL = process.env.NEXT_PUBLIC_DOMAIN_URL;
+
+      try {
+        const res = await fetch(`${domainURL}/notification/unread-count`, {
+          credentials: "include",
+          mode: "cors",
+          method: "GET",
+        });
+
+        const resData = await res.json();
+
+        if (res.status >= 400) {
+          console.log(resData.message);
+        }
+
+        if (res.status >= 200 && res.status <= 300) {
+          console.log({
+            unreadNotificatioCount: resData.unreadNotificationCount,
+          });
+          setNotificationCount(resData.unreadNotificationCount);
+        }
+      } catch (err) {
+        if (err instanceof Error) console.log(err.message);
+      }
+    };
+
+    if (user && notificationCount === null) {
+      fetchNotificationCount(user.id);
+
+      socket.emit("subscribe", `notification:${user.id}`);
+      socket.on(
+        "notificationupdate",
+        fetchNotificationCount.bind(this, user.id)
+      );
+
+      socket.on(
+        "initiate",
+        connectToRoom.bind(this, socket, `notification:${user.id}`)
+      );
+    }
+
+    return () => {
+      if (user) {
+        socket.off("initiate");
+        socket.off(
+          "notificationupdate",
+          fetchNotificationCount.bind(this, user.id)
+        );
+        socket.emit("unsubscribe", `notification:${user.id}`);
+      }
+    };
+  }, [user, notificationCount]);
+
+  return (
+    <NotificationCountContext
+      value={{ notificationCount, setNotificationCount }}
+    >
+      {children}
+    </NotificationCountContext>
+  );
+};
+
+export default NotificationCountProvider;
