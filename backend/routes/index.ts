56145aee303867d0c8eea1ddd7fc18fe6c43970c
Adhisafdhol <aadheusx@gmail.com>
2025-07-27 13:12:27 +0700

    feat(routes): implement notifications GET route

--- backend/routes/index.ts
+++ backend/routes/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import express from "express";
 import * as communityController from "../controllers/community";
+import * as notificationController from "../controllers/notification";
 const router = express.Router();
 
 /* GET home page. */
@@ -9,5 +10,7 @@ router.get("/", function (req: Request, res: Response) {
 });
 
 router.get("/communities", communityController.communities_get);
+
+router.get("/notifications", notificationController.notifications_get);
 
 export default router;
