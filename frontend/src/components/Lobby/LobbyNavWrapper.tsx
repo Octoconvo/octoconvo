51327a63e5b88d63e18b802609f0137fa20ffc13
Adhisafdhol <aadheusx@gmail.com>
2025-07-29 14:17:01 +0700

    feat: implement notification modal in the lobby
    Implement notification modal in the lobby wrapper and its related
    functionality.

--- frontend/src/components/Lobby/LobbyNavWrapper.tsx
+++ frontend/src/components/Lobby/LobbyNavWrapper.tsx
@@ -1,21 +1,43 @@
 "use client";
 
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 import LobbyNav from "./LobbyNav";
 import ProfileModal from "@/components/ProfileModal";
 import { UserProfileContext } from "@/contexts/user";
-import { UserProfileModalContext } from "@/contexts/modal";
+import {
+  UserProfileModalContext,
+  NotificationModalContext,
+} from "@/contexts/modal";
+import NotificationModal from "@/components/Notification/NotificationModal";
 
 const LobbyNavWrapper = () => {
   const { userProfile } = useContext(UserProfileContext);
   const userProfileModal = useRef<null | HTMLDivElement>(null);
+  const notificationModal = useRef<null | HTMLDivElement>(null);
+  const [isNotificationModalOpen, setIsNotificationModalOpen] =
+    useState<boolean>(false);
+  const [isNotificationModalAnimating, setIsNotificationModalAnimating] =
+    useState<boolean>(false);
 
   return (
     <div className="relative z-10">
-      <UserProfileModalContext.Provider value={{ userProfileModal }}>
-        <LobbyNav />
-        <ProfileModal profileData={userProfile} />
-      </UserProfileModalContext.Provider>
+      <NotificationModalContext
+        value={{
+          notificationModal,
+          toggleNotificationModalView: () => {
+            setIsNotificationModalAnimating(true);
+            setIsNotificationModalOpen(!isNotificationModalOpen);
+          },
+          isNotificationModalOpen,
+          isNotificationModalAnimating,
+        }}
+      >
+        <UserProfileModalContext.Provider value={{ userProfileModal }}>
+          <LobbyNav />
+          <ProfileModal profileData={userProfile} />
+          <NotificationModal />
+        </UserProfileModalContext.Provider>
+      </NotificationModalContext>
     </div>
   );
 };
