3c4a94f8f2136ef244415bea5dbf3f6adae24b86
Adhisafdhol <aadheusx@gmail.com>
2025-07-29 13:53:14 +0700

    feat: implement NotificationNav onClick fn
    - add onClick function that toggles the notification modal view.
    - change the notification element type from Link to Button to better
    match its current usage.

--- frontend/src/components/Lobby/NotificationNav.tsx
+++ frontend/src/components/Lobby/NotificationNav.tsx
@@ -1,51 +1,56 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NotificationCountContext } from "@/contexts/notification";
+import NotificationModal from "@/components/Notification/NotificationModal";
+import { NotificationModalContext } from "@/contexts/modal";
 
 const NotificationNav = () => {
   const path = usePathname();
   const { notificationCount } = useContext(NotificationCountContext);
-
-  console.log({ path });
+  const { toggleNotificationModalView } = useContext(NotificationModalContext);
   return (
-    <div className="relative">
-      <Link
-        data-testid="notification-l"
-        href={`/lobby/notification`}
-        className={
-          "flex relative items-center justify-center h-12 rounded-[8px]" +
-          " before:rounded-[8px] transition-all hover:transition-all" +
-          " before:bg-gr-brand-1-2-1-3-d w-[48px] h-[48px] " +
-          " before:hover:bg-gr-brand-1-2-1-d before:h-full before:w-full" +
-          (path && path.split("/").includes("notification")
-            ? " before:bg-gr-brand-1-3-d-1-b before:w-full before:h-full" +
-              " before:rounded-[8px] before:scale-110 hover:shadow-none" +
-              " hover:before:bg-gr-brand-1-2-3-d-1-t hover:before:scale-110"
-            : "")
-        }
-      >
-        <span
+    <>
+      <div className="relative">
+        <button
+          data-testid="notification-l"
+          onClick={() => {
+            toggleNotificationModalView();
+          }}
           className={
-            "lobby-nav-icon" + " after:bg-[url(/images/notification-icon.svg)]"
-          }
-        ></span>
-      </Link>
-      {notificationCount ? (
-        <div
-          data-testid="nrd-ntfctn-cnt-indicator"
-          className={
-            "absolute right-0 top-0 flex justify-center items-center" +
-            " bg-notification w-[36px] h-[36px] rounded-full" +
-            " translate-y-[-50%] translate-x-[50%] animate-notification"
+            "flex relative items-center justify-center h-12 rounded-[8px]" +
+            " before:rounded-[8px] transition-all hover:transition-all" +
+            " before:bg-gr-brand-1-2-1-3-d w-[48px] h-[48px] " +
+            " before:hover:bg-gr-brand-1-2-1-d before:h-full before:w-full" +
+            (path && path.split("/").includes("notification")
+              ? " before:bg-gr-brand-1-3-d-1-b before:w-full before:h-full" +
+                " before:rounded-[8px] before:scale-110 hover:shadow-none" +
+                " hover:before:bg-gr-brand-1-2-3-d-1-t hover:before:scale-110"
+              : "")
           }
         >
-          <p className="text-white-100">{notificationCount}</p>
-        </div>
-      ) : null}
-    </div>
+          <span
+            className={
+              "lobby-nav-icon" +
+              " after:bg-[url(/images/notification-icon.svg)]"
+            }
+          ></span>
+        </button>
+        {notificationCount ? (
+          <div
+            data-testid="nrd-ntfctn-cnt-indicator"
+            className={
+              "absolute right-0 top-0 flex justify-center items-center" +
+              " bg-notification w-[36px] h-[36px] rounded-full" +
+              " translate-y-[-50%] translate-x-[50%] animate-notification"
+            }
+          >
+            <p className="text-white-100">{notificationCount}</p>
+          </div>
+        ) : null}
+      </div>
+    </>
   );
 };
 
