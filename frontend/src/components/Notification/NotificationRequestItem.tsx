a38074b0ae7ef6b33e352acb3f9aa34844d077ac
Adhisafdhol <aadheusx@gmail.com>
2025-07-29 12:43:58 +0700

    feat(components): add NotificationRequestItem
    Add a component to render a notification where the notification's type
    is of a request type.

---
+++ frontend/src/components/Notification/NotificationRequestItem.tsx
@@ -0,0 +1,79 @@
+import { NotificationGET } from "@/types/response";
+import { capitaliseStringFirstLetter } from "@/utils/string";
+
+const NotificationRequestItem = ({
+  notification,
+}: {
+  notification: NotificationGET;
+}) => {
+  return (
+    <li
+      data-testid="ntfctn-rqst-itm"
+      className={
+        "flex flex-col p-[16px] rounded-[4px] w-full" +
+        (notification.isRead ? " bg-gr-black-1-b" : " bg-brand-1-2")
+      }
+    >
+      <p
+        data-testid="ntfctn-rqst-itm-msg"
+        className=" max-w-full text-p font-regular"
+      >
+        <span
+          data-testid="ntfctn-rqst-itm-msg-usr-usrnm"
+          className="break-words font-bold text-white-100"
+        >
+          {notification.triggeredBy.username}
+        </span>
+        <span
+          data-testid="ntfctn-rqst-itm-msg-pyld"
+          className="font-regular text-white-200 "
+        >
+          {" " + notification.payload + " "}
+        </span>
+        <span
+          data-testid="ntfctn-rqst-itm-msg-cmmnty-nm"
+          className="break-words font-bold text-white-100"
+        >
+          {notification.type === "COMMUNITYREQUEST" &&
+            notification.community &&
+            notification.community.name}
+        </span>
+      </p>
+      {(notification.status === "REJECTED" ||
+        notification.status === "ACCEPTED") && (
+        <p data-testid="ntfctn-rqst-itm-sts-updt">
+          {capitaliseStringFirstLetter(notification.status)}
+        </p>
+      )}
+      {notification.status === "PENDING" && (
+        <div
+          data-testid="ntfctn-rqst-itm-btn-lst"
+          className="flex gap-[16px] self-end"
+        >
+          <button
+            className={
+              "px-[16px] py-[4px] font-regular rounded-[4px] text-p" +
+              " hover:bg-brand-4" +
+              (notification.isRead
+                ? " bg-grey-100 hover:text-grey-100"
+                : " bg-black-500 hover:text-black-500")
+            }
+          >
+            Accept
+          </button>
+          <button
+            className={
+              "px-[16px] py-[4px] font-regular rounded-[4px] text-p" +
+              " hover:bg-invalid" +
+              (notification.isRead ? " bg-grey-100" : " bg-black-500")
+            }
+          >
+            Reject
+          </button>
+        </div>
+      )}
+    </li>
+  );
+};
+
+export default NotificationRequestItem;
