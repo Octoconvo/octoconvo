a73eaf8cd5fd17fc6f467e35414171a260fab1bb
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-03 13:33:46 +0700

    test(component): add RequestActionBtn tests

---
+++ frontend/__tests__/components/Notification/RequestActionBtn.test.tsx
@@ -0,0 +1,64 @@
+import { screen, render, act } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import RequestActionBtn from "@/components/Notification/RequestActionBtn";
+
+const notification = jest.fn();
+const participant = jest.fn();
+const newNotifications = jest.fn();
+const onSubmitMock = jest.fn((action: string) => {});
+
+describe("Render RequestActionBtn with false isRead and REJECT action", () => {
+  beforeEach(async () => {
+    await act(async () =>
+      render(
+        <RequestActionBtn
+          isRead={false}
+          action="REJECT"
+          onSubmit={onSubmitMock}
+        />
+      )
+    );
+  });
+
+  const user = userEvent.setup();
+  test(
+    "Call the onSubmit with REJECT as the argument if the action is REJECT" +
+      " when the button is clicked",
+    async () => {
+      const btn = screen.getByTestId("ntfctn-rqst-actn-btn");
+      expect(onSubmitMock).toHaveBeenCalledTimes(0);
+
+      await user.click(btn);
+      expect(onSubmitMock).toHaveBeenCalledWith("REJECT");
+    }
+  );
+});
+
+describe("Render RequestActionBtn with true isRead and ACCEPT action", () => {
+  beforeEach(async () => {
+    await act(async () =>
+      render(
+        <RequestActionBtn
+          isRead={true}
+          action="ACCEPT"
+          onSubmit={onSubmitMock}
+        />
+      )
+    );
+  });
+
+  const user = userEvent.setup();
+  test(
+    "Call the onSubmit with REJECT as the argument if the action is REJECT" +
+      " when the button is clicked",
+    async () => {
+      onSubmitMock.mockClear();
+      const btn = screen.getByTestId("ntfctn-rqst-actn-btn");
+      expect(onSubmitMock).toHaveBeenCalledTimes(0);
+
+      await user.click(btn);
+      expect(onSubmitMock).toHaveBeenCalledWith("ACCEPT");
+    }
+  );
+});
