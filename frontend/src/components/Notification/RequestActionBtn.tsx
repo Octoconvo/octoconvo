54abdc1927291cea0218065f8bf315fefa066d6b
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-03 13:34:49 +0700

    feat(component): add RequestActionBtn
    Add a component of a button to handle notification requests' accept and
    reject actions.

---
+++ frontend/src/components/Notification/RequestActionBtn.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { capitaliseStringFirstLetter } from "@/utils/string";
+
+const RequestActionBtn = ({
+  isRead,
+  onSubmit,
+  action,
+}: {
+  isRead: boolean;
+  action: "ACCEPT" | "REJECT";
+  onSubmit: (action: "ACCEPT" | "REJECT") => void;
+}) => {
+  return (
+    <button
+      data-testid="ntfctn-rqst-actn-btn"
+      onClick={() => {
+        onSubmit(action);
+      }}
+      className={
+        "px-[16px] py-[4px] font-regular rounded-[4px] text-p" +
+        " hover:bg-brand-4" +
+        (isRead
+          ? " bg-grey-100 hover:text-grey-100"
+          : " bg-black-500 hover:text-black-500")
+      }
+    >
+      {capitaliseStringFirstLetter(action)}
+    </button>
+  );
+};
+
+export default RequestActionBtn;
