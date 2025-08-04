fb6c0626aab45810852a4609dc2fcceb3bcf5045
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-04 13:31:15 +0700

    feat(component): add notification Payload
    Add a component to display the notification's payload.

---
+++ frontend/src/components/Notification/Payload.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { unescapeString } from "@/utils/string";
+
+const Payload = ({
+  triggeredBy,
+  triggeredFor,
+  payload,
+}: {
+  triggeredBy: string;
+  triggeredFor: string | null;
+  payload: string;
+}) => {
+  return (
+    <>
+      <p
+        data-testid="ntfctn-rqst-itm-msg"
+        className=" max-w-full text-p font-regular"
+      >
+        <span
+          data-testid="ntfctn-rqst-itm-msg-trggrdby"
+          className="break-words font-bold text-white-100"
+        >
+          {unescapeString(triggeredBy)}
+        </span>
+        <span
+          data-testid="ntfctn-rqst-itm-msg-pyld"
+          className="font-regular text-white-200 "
+        >
+          {" " + payload + " "}
+        </span>
+        {triggeredFor && (
+          <span
+            data-testid="ntfctn-rqst-itm-msg-trggrdfr"
+            className="break-words font-bold text-white-100"
+          >
+            {unescapeString(triggeredFor)}
+          </span>
+        )}
+      </p>
+    </>
+  );
+};
+
+export default Payload;
