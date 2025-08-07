0bb35d27cee749f828bb7a452c6ca0ca90dd0344
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-07 21:45:48 +0700

    feat(api): add notificationReadStatusPOST
    Add a fetch request to update the notifications' read statuses.

---
+++ frontend/src/api/notification.tsx
@@ -0,0 +1,47 @@
+import { NotificationGET } from "@/types/response";
+
+const notificationsReadStatusPOST = async ({
+  notifications,
+  onSuccess,
+}: {
+  notifications: NotificationGET[];
+  onSuccess: ({ data }: { data: NotificationGET[] }) => void;
+}) => {
+  const domainURL = process.env.NEXT_PUBLIC_DOMAIN_URL;
+
+  const firstNotification = notifications[0];
+  const lastNotification = notifications[notifications.length - 1];
+
+  try {
+    const formData = new URLSearchParams();
+    formData.append("startdate", firstNotification.createdAt);
+    formData.append("enddate", lastNotification.createdAt);
+
+    const res = await fetch(
+      `${domainURL}/notifications/update-read-status?mode=SILENT`,
+      {
+        method: "POST",
+        mode: "cors",
+        credentials: "include",
+        body: formData,
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded ",
+        },
+      }
+    );
+
+    const resData = await res.json();
+
+    if (res.status >= 400) {
+      console.log(resData.message);
+    }
+
+    if (res.status >= 200 && res.status <= 300) {
+      onSuccess({ data: resData.notifications });
+    }
+  } catch (err) {
+    if (err instanceof Error) console.log(err.message);
+  }
+};
+
+export { notificationsReadStatusPOST };
