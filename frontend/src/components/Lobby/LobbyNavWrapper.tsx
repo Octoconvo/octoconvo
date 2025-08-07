056021a709ee59107384e47def5fe67fa8492958
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-07 22:35:32 +0700

    refactor: update LobbyNavWrapper component
    Remove notification related states and contexts from the
    LobbyNavWrapper.

--- frontend/src/components/Lobby/LobbyNavWrapper.tsx
+++ frontend/src/components/Lobby/LobbyNavWrapper.tsx
@@ -1,43 +1,23 @@
 "use client";
 
-import { useContext, useRef, useState } from "react";
+import { useContext, useRef } from "react";
 import LobbyNav from "./LobbyNav";
 import ProfileModal from "@/components/ProfileModal";
 import { UserProfileContext } from "@/contexts/user";
-import {
-  UserProfileModalContext,
-  NotificationModalContext,
-} from "@/contexts/modal";
+import { UserProfileModalContext } from "@/contexts/modal";
 import NotificationModal from "@/components/Notification/NotificationModal";
 
 const LobbyNavWrapper = () => {
   const { userProfile } = useContext(UserProfileContext);
   const userProfileModal = useRef<null | HTMLDivElement>(null);
-  const notificationModal = useRef<null | HTMLDivElement>(null);
-  const [isNotificationModalOpen, setIsNotificationModalOpen] =
-    useState<boolean>(false);
-  const [isNotificationModalAnimating, setIsNotificationModalAnimating] =
-    useState<boolean>(false);
 
   return (
     <div className="relative z-10">
-      <NotificationModalContext
-        value={{
-          notificationModal,
-          toggleNotificationModalView: () => {
-            setIsNotificationModalAnimating(true);
-            setIsNotificationModalOpen(!isNotificationModalOpen);
-          },
-          isNotificationModalOpen,
-          isNotificationModalAnimating,
-        }}
-      >
-        <UserProfileModalContext.Provider value={{ userProfileModal }}>
-          <LobbyNav />
-          <ProfileModal profileData={userProfile} />
-          <NotificationModal />
-        </UserProfileModalContext.Provider>
-      </NotificationModalContext>
+      <UserProfileModalContext.Provider value={{ userProfileModal }}>
+        <LobbyNav />
+        <ProfileModal profileData={userProfile} />
+        <NotificationModal />
+      </UserProfileModalContext.Provider>
     </div>
   );
 };
