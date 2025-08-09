3976b56fe380fd2924925fef9bbbd99b6d5a6018
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-09 14:54:41 +0700

    test: update CreateCommunity image preview tests
    Update the CreateCommunity component's tests for the image preview
    refactor.

--- frontend/__tests__/components/CreateCommunity/CreateCommunityForm.test.tsx
+++ frontend/__tests__/components/CreateCommunity/CreateCommunityForm.test.tsx
@@ -2,7 +2,7 @@ import CreateCommunityForm from "@/components/CreateCommunity/CreateCommunityFor
 import "@testing-library/jest-dom";
 import { render, screen, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { previewFile, selectFile } from "@/utils/file";
+import { previewFile, selectFile, validateFiles } from "@/utils/file";
 import { triggerInputClick } from "@/utils/controller";
 
 const onSubmit = jest.fn();
@@ -14,6 +14,8 @@ const imageFile = new File(["a".repeat(1024)], "image-test", {
 jest.mock("@/utils/file", () => ({
   selectFile: jest.fn(),
   previewFile: jest.fn(),
+  validateFiles: jest.fn(() => ["testimage1"]),
+  previewImage: jest.fn(() => "buffer:testimagepreviewurl1"),
 }));
 
 jest.mock("@/utils/controller", () => ({
@@ -36,18 +38,26 @@ describe("Render CreateCommunityForm to test selectFile invocation on file selec
     );
   });
 
-  it("selectFile is invoked on avatar file Input", async () => {
+  it("Update avatar preview after selecting an image", async () => {
     const input = screen.queryByTestId("crt-cmmnty-avatar") as HTMLInputElement;
     expect(input).toBeDefined();
+
     await user.upload(input, imageFile);
-    expect(selectFile).toHaveBeenCalled();
+
+    const avatarPreview = screen.getByTestId(
+      "crt-cmmnty-avatar-img"
+    ) as HTMLImageElement;
+    expect(avatarPreview.src).toContain("testimagepreviewurl1");
   });
 
-  it("selectFile is invoked on banner file Input", async () => {
+  it("Update banner preview after selecting an image", async () => {
     const input = screen.queryByTestId("crt-cmmnty-banner") as HTMLInputElement;
     expect(input).toBeDefined();
     await user.upload(input, imageFile);
-    expect(selectFile).toHaveBeenCalled();
+    const bannerPreview = screen.getByTestId(
+      "crt-cmmnty-banner-img"
+    ) as HTMLImageElement;
+    expect(bannerPreview.src).toContain("testimagepreviewurl1");
   });
 
   it("Invoke avatar file input when avatar button is clicked", async () => {
@@ -98,45 +108,14 @@ describe("Render CreateCommunityForm to test validationError", () => {
     expect(error).toBeDefined();
   });
 
-  it("ResetError is called onInput when validationError is not empty", async () => {
-    const nameInput = screen.getByTestId("crt-cmmnty-name");
+  it(
+    "ResetError is called onInput when validationError is not" + " empty",
+    async () => {
+      const nameInput = screen.getByTestId("crt-cmmnty-name");
 
-    expect(nameInput).toBeInTheDocument();
-    await user.type(nameInput, "A");
-    expect(resetError).toHaveBeenCalled();
-  });
-});
-
-describe("Render CreateCommunityProfile and reset mock to test previewFile invocations", () => {
-  const user = userEvent.setup();
-
-  beforeEach(async () => {
-    const originalModule = jest.requireActual("@/utils/file");
-    (selectFile as jest.Mock).mockImplementationOnce(originalModule.selectFile);
-
-    await act(async () =>
-      render(
-        <CreateCommunityForm
-          isSubmitting={false}
-          onSubmit={onSubmit}
-          validationError={[]}
-          resetError={() => {}}
-        ></CreateCommunityForm>
-      )
-    );
-  });
-
-  it("previewFile is Invoked when avatar is not null", async () => {
-    const input = screen.queryByTestId("crt-cmmnty-avatar") as HTMLInputElement;
-    expect(input).toBeDefined();
-    await user.upload(input, imageFile);
-    expect(previewFile).toHaveBeenCalled();
-  });
-
-  it("previewFile is invoked when banner is not null", async () => {
-    const input = screen.queryByTestId("crt-cmmnty-banner") as HTMLInputElement;
-    expect(input).toBeDefined();
-    await user.upload(input, imageFile);
-    expect(previewFile).toHaveBeenCalled();
-  });
+      expect(nameInput).toBeInTheDocument();
+      await user.type(nameInput, "A");
+      expect(resetError).toHaveBeenCalled();
+    }
+  );
 });
