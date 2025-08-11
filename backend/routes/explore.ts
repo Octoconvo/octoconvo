e6f27e19225c17aea3d4e1f43145e8ee769fa71f
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-11 18:26:47 +0700

    feat: add explore profile GET route

--- backend/routes/explore.ts
+++ backend/routes/explore.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import * as communityController from "../controllers/community";
+import * as profileController from "../controllers/profile";
 
 const router = express.Router();
 
 router.get("/communities", communityController.communities_explore_get);
+
+router.get("/profiles", profileController.profiles_explore_get);
 
 export default router;
