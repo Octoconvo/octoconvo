7de3ddbe15ad3484134a80c7d4b5281af0a0c777
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-12 17:37:57 +0700

    feat(component): add ProfileItem
    Add ProfileItem component to render every profile on the explore  page.

---
+++ frontend/src/components/Explore/ProfileItem.tsx
@@ -0,0 +1,56 @@
+import { ProfileAPI } from "@/types/response";
+
+const ProfileItem = ({ profile }: { profile: ProfileAPI }) => {
+  return (
+    <li className="flex flex-col bg-black-100 p-[32px] rounded-8 h-[150px]">
+      <button className="flex gap-[32px] h-full items-center">
+        <figure className="bg-gr-black-2-r min-w-[64px] h-[64px] rounded-full">
+          {profile.avatar && (
+            <img
+              data-testid="profile-item-avatar"
+              className={
+                "object-center object-cover w-full" +
+                " w-full h-full rounded-[inherit]"
+              }
+              src={profile.avatar}
+            />
+          )}
+        </figure>
+        <div className="flex flex-col gap-[16px] justify-between overflow-hidden">
+          <div className="flex flex-col gap-[4px]">
+            <p
+              data-testid="profile-item-displayname"
+              className={
+                "text-white-100 text-start text-ellipsis}" +
+                " text-nowrap overflow-hidden"
+              }
+            >
+              {profile.displayName}
+            </p>
+            <p
+              data-testid="profile-item-username"
+              className={
+                "text-white-200 text-s text-start" +
+                " text-ellipsis text-nowrap overflow-hidden"
+              }
+            >
+              @{profile.username}
+            </p>
+          </div>
+
+          <p
+            data-testid="profile-item-bio"
+            className={
+              "text-white-200 text-start text-ellipsis" +
+              " text-nowrap overflow-hidden"
+            }
+          >
+            {profile.bio ? profile.bio : "No bio yet"}
+          </p>
+        </div>
+      </button>
+    </li>
+  );
+};
+
+export default ProfileItem;
