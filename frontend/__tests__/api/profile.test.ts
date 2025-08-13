018fde5704873965358364f25f7d435de9bfca60
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-13 18:10:33 +0700

    test: add tests for getProfileFromAPI function

---
+++ frontend/__tests__/api/profile.test.ts
@@ -0,0 +1,44 @@
+import { getProfilesFromAPI } from "@/api/profile";
+import { ProfileAPI } from "@/types/response";
+
+const profile: ProfileAPI = {
+  id: "testprofile1",
+  username: "testprofile1",
+  displayName: "testprofile1",
+  avatar: null,
+  bio: null,
+  banner: null,
+  isDeleted: false,
+  lastSeen: "testprofile1",
+  createdAt: "testprofile1",
+  updatedAt: "testprofile1",
+};
+const message = "Successfully fetched Data";
+const nextCursor = "testnextCursor1";
+
+global.fetch = jest.fn((_url) =>
+  Promise.resolve({
+    status: 200,
+    json: () =>
+      Promise.resolve({
+        nextCursor: nextCursor,
+        profiles: [profile],
+        message,
+      }),
+  })
+) as jest.Mock;
+
+describe("Test getProfilesFromAPI", () => {
+  test("getProfilesFromAPI return the correct data", async () => {
+    const getProfilesResponse = await getProfilesFromAPI({
+      name: "testname1",
+    });
+
+    expect(getProfilesResponse).toStrictEqual({
+      message,
+      status: 200,
+      profiles: [profile],
+      nextCursor,
+    });
+  });
+});
