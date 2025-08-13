59bf3d2a5de9afbf2d5ece2d0eca3fcc00f89510
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-13 18:46:46 +0700

    test: add ProfileBox's tests

---
+++ frontend/__tests__/components/Explore/ProfileBox.test.tsx
@@ -0,0 +1,37 @@
+import ProfileBox from "@/components/Explore/ProfileBox";
+import { render, screen } from "@testing-library/react";
+import { ProfileAPI } from "@/types/response";
+
+const profile: ProfileAPI = {
+  id: "testprofile1",
+  username: "testprofile1",
+  displayName: "testprofile1",
+  avatar: "testprofile1",
+  bio: null,
+  banner: null,
+  isDeleted: false,
+  lastSeen: "testprofile1",
+  createdAt: "testprofile1",
+  updatedAt: "testprofile1",
+};
+
+window.IntersectionObserver = jest.fn(() => ({
+  observe: jest.fn(),
+  unobserve: jest.fn(),
+})) as jest.Mock;
+
+describe("Render ProfileBox", () => {
+  test("Render the profiles", () => {
+    render(
+      <ProfileBox
+        profiles={[profile]}
+        nameQuery=""
+        nextCursor={false}
+        updateProfilesStates={jest.fn()}
+      />
+    );
+
+    const lists = screen.getAllByRole("list");
+    expect(lists.length).toBe(1);
+  });
+});
