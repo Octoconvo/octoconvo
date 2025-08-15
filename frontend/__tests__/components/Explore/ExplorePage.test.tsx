74d50217b8d3dbfc7815878c69c59d0fadfbd32c
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-15 18:01:38 +0700

    test: test ExplorePage user and community buttons
    Test the ExplorePage's navigation to fetch and display the profiles or
    communities data depending on the current view.

--- frontend/__tests__/components/Explore/ExplorePage.test.tsx
+++ frontend/__tests__/components/Explore/ExplorePage.test.tsx
@@ -2,7 +2,8 @@ import ExplorePage from "@/components/Explore/ExplorePage";
 import userEvent from "@testing-library/user-event";
 import { render, act, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { CommunityExploreGET } from "@/types/response";
+import { CommunityExploreGET, ProfileAPI } from "@/types/response";
+import SearchBar from "@/components/SearchBar/SearchBar";
 
 const community1: CommunityExploreGET = {
   id: "testid1",
@@ -18,21 +19,48 @@ const community1: CommunityExploreGET = {
   updatedAt: "2025-03-12T20:09:55.245Z",
 };
 
+const profile1: ProfileAPI = {
+  id: "testprofile1",
+  username: "testprofile1",
+  displayName: "testprofile1",
+  avatar: null,
+  bio: null,
+  banner: null,
+  isDeleted: false,
+  lastSeen: "testprofile1",
+  createdAt: "testprofile1",
+  updatedAt: "testprofile1",
+};
+
+const response = {
+  communities: {
+    message: "Successfully fetched communities",
+    communities: [community1],
+    nextCursor: false,
+  },
+  profiles: {
+    message: "Successfully fetched profiles",
+    profiles: [profile1],
+    nextCursor: false,
+  },
+};
+
 global.fetch = jest.fn().mockImplementation(
-  jest.fn(
-    (
-      //eslint-disable-next-line
-      _url
-    ) =>
-      Promise.resolve().then(() => ({
-        status: 200,
-        json: () =>
-          Promise.resolve({
-            communities: [community1],
-            cursor: false,
-          }),
-      }))
-  )
+  jest.fn((_url) => {
+    const path = _url.split("/");
+    const lastPath = path[path.length - 1].split("?")[0];
+    type ValidPath = "communities" | "profiles";
+    const validPath = ["communities", "profiles"];
+
+    const data = validPath.includes(lastPath)
+      ? response[lastPath as ValidPath]
+      : {};
+
+    return Promise.resolve().then(() => ({
+      status: 200,
+      json: () => Promise.resolve(data),
+    }));
+  })
 );
 
 window.IntersectionObserver = jest.fn(() => ({
@@ -51,6 +79,12 @@ describe("Render ExplorePage", () => {
     const communityBox = screen.getByTestId("cmmnty-bx-ulst");
 
     expect(communityBox).toBeInTheDocument();
+  });
+
+  test("The featured data's length should be 1", () => {
+    const communityBox = screen.getByTestId("cmmnty-bx-ulst");
+
+    expect(communityBox.children.length).toBe(1);
   });
 
   test("Hide explore page navigation on featured data view", () => {
@@ -109,29 +143,35 @@ describe("Render ExplorePage", () => {
     expect(screen.queryByTestId("explr-pg-cmmnty-btn")).not.toBeInTheDocument();
   });
 
-  test("Don't render community modal when the active community is null", async () => {
-    const communityModalContainer = screen.queryByTestId(
-      "xplr-cmmnty-mdl-cntnr"
-    );
+  test(
+    "Don't render community modal when the active community" + " is null",
+    async () => {
+      const communityModalContainer = screen.queryByTestId(
+        "xplr-cmmnty-mdl-cntnr"
+      );
 
-    expect(communityModalContainer).not.toBeInTheDocument();
-  });
+      expect(communityModalContainer).not.toBeInTheDocument();
+    }
+  );
 
-  test("render community modal when the active community is not null", async () => {
-    const communityItemBtns = screen.getAllByTestId("xplr-cmmnty-itm-btn");
+  test(
+    "Render community modal when the active community is" + " not null",
+    async () => {
+      const communityItemBtns = screen.getAllByTestId("xplr-cmmnty-itm-btn");
 
-    expect(communityItemBtns.length).toBeGreaterThan(0);
+      expect(communityItemBtns.length).toBeGreaterThan(0);
 
-    await user.click(communityItemBtns[0]);
+      await user.click(communityItemBtns[0]);
 
-    const communityModalContainer = screen.queryByTestId(
-      "xplr-cmmnty-mdl-cntnr"
-    );
+      const communityModalContainer = screen.queryByTestId(
+        "xplr-cmmnty-mdl-cntnr"
+      );
 
-    expect(communityModalContainer).toBeInTheDocument();
-  });
+      expect(communityModalContainer).toBeInTheDocument();
+    }
+  );
 
-  test("close community modal after clicking the Escape key", async () => {
+  test("Close community modal after clicking the Escape key", async () => {
     const communityItemBtns = screen.getAllByTestId("xplr-cmmnty-itm-btn");
 
     expect(communityItemBtns.length).toBeGreaterThan(0);
@@ -148,5 +188,41 @@ describe("Render ExplorePage", () => {
     expect(
       screen.queryByTestId("xplr-cmmnty-mdl-cntnr")
     ).not.toBeInTheDocument();
+  });
+});
+
+describe("Test ExplorePage buttons and fetches", () => {
+  const user = userEvent.setup();
+
+  beforeEach(async () => {
+    await act(() => render(<ExplorePage />));
+  });
+
+  test("Render CommunityBox after successful submit", async () => {
+    const nameInput = screen.getByTestId("srchbr-nm-input");
+    const searchBtn = screen.getByTestId("srchbr-sbmt-btn");
+
+    await user.type(nameInput, "testname1");
+    await user.click(searchBtn);
+
+    const explorePageNav = screen.getByTestId("explr-pg-nav");
+    const communityBox = screen.getByTestId("cmmnty-bx-ulst");
+
+    expect(communityBox).toBeInTheDocument();
+    expect(explorePageNav).toBeInTheDocument();
+  });
+
+  test("Render ProfileBox after clicking user button", async () => {
+    const nameInput = screen.getByTestId("srchbr-nm-input");
+    const searchBtn = screen.getByTestId("srchbr-sbmt-btn");
+
+    await user.type(nameInput, "testname1");
+    await user.click(searchBtn);
+
+    const explorePageUserBtn = screen.getByTestId("explr-pg-usr-btn");
+    await user.click(explorePageUserBtn);
+
+    const profileBox = screen.getByTestId("profile-box-ulist");
+    expect(profileBox).toBeInTheDocument();
   });
 });
