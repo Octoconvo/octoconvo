285a4481419a3b90999a8220b29f9f78b5017fd5
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-15 16:33:16 +0700

    test: add getCommunityFromAPI's tests

---
+++ frontend/__tests__/api/community.test.ts
@@ -0,0 +1,46 @@
+import { getCommunitiesFromAPI } from "@/api/community";
+import { CommunityExploreGET } from "@/types/response";
+
+const community: CommunityExploreGET = {
+  id: "testcommunity1",
+  name: "testcommunity1",
+  bio: null,
+  avatar: null,
+  banner: null,
+  isDeleted: false,
+  createdAt: "testcommunity1",
+  updatedAt: "testcommunity1",
+  _count: {
+    participants: 1,
+  },
+};
+
+const message = "Successfully fetched data";
+const nextCursor = "testnextcursor1";
+
+global.fetch = jest.fn((_url) =>
+  Promise.resolve({
+    status: 200,
+    json: () =>
+      Promise.resolve({
+        nextCursor: nextCursor,
+        communities: [community],
+        message,
+      }),
+  })
+) as jest.Mock;
+
+describe("Test getCommunitiesFromAPI", () => {
+  test("getCommunitiesFromAPI return the correct data", async () => {
+    const getCommunitiesResponse = await getCommunitiesFromAPI({
+      name: "testname1",
+    });
+
+    expect(getCommunitiesResponse).toStrictEqual({
+      message,
+      status: 200,
+      communities: [community],
+      nextCursor,
+    });
+  });
+});
