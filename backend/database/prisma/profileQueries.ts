7c0386908a77a4e6966b6622a48f43bd5768646d
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-22 19:31:12 +0700

    fix: use findUnique method in getUserProfileById
    Use findUnique instead of findFirst to ensure that prisma will throw
    error if the id is invalid.

--- backend/database/prisma/profileQueries.ts
+++ backend/database/prisma/profileQueries.ts
@@ -1,7 +1,7 @@
 import prisma from "./client";
 
 const getUserProfileById = async (id: string) => {
-  const userProfile = await prisma.user.findFirst({
+  const userProfile = await prisma.user.findUnique({
     where: {
       id: id,
     },
