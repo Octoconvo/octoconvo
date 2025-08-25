5e57cdf886d7b8803131a39a59a7301a55a461aa
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-25 19:22:15 +0700

    feat(util): add postFriendToAPI

--- frontend/src/utils/api/friend.ts
+++ frontend/src/utils/api/friend.ts
@@ -1,4 +1,4 @@
-import { ErrorAPI } from "@/types/api";
+import { ErrorAPI, FriendAPI } from "@/types/api";
 
 const DOMAIN_URL = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
@@ -33,4 +33,36 @@ const getFriendshipStatusFromAPI = async ({
   };
 };
 
-export { getFriendshipStatusFromAPI };
+type PostFriendToAPI = {
+  formData: URLSearchParams;
+};
+
+type PostFriendToAPIData = {
+  status: number;
+  message: string;
+  error?: ErrorAPI;
+  friends: FriendAPI[];
+};
+
+const postFriendToAPI = async ({
+  formData,
+}: PostFriendToAPI): Promise<PostFriendToAPIData> => {
+  const response = await fetch(`${DOMAIN_URL}/friend`, {
+    method: "POST",
+    credentials: "include",
+    mode: "cors",
+    body: formData,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+
+  const responseData = await response.json();
+
+  return {
+    status: response.status,
+    ...responseData,
+  };
+};
+
+export { getFriendshipStatusFromAPI, postFriendToAPI };
