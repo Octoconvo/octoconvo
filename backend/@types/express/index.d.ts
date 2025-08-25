a0087842a6d98c932b260b79f26532366a86a603
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-25 18:25:30 +0700

    feat(type): add profile type to the Express User

--- backend/@types/express/index.d.ts
+++ backend/@types/express/index.d.ts
@@ -1,4 +1,7 @@
 import UserValue from "../user";
+import { User as PrismaUser } from "@prisma/client";
+
+type Profile = Omit<PrismaUser, "password", "isDeleted">;
 
 declare global {
   namespace Express {
@@ -6,6 +9,7 @@ declare global {
     interface User extends UserValue {}
     interface Request {
       user?: UserValue;
+      friend?: Profile;
     }
   }
 }
