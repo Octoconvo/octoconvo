09e87fe14603b753e446164ffe0c2eb7bcd438c5
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-25 18:37:16 +0700

    feat(controller): add friend_add_post
    Add a controller to send a friend request.

--- backend/controllers/friend.ts
+++ backend/controllers/friend.ts
@@ -4,7 +4,11 @@ import { createAuthenticationMiddleware } from "../utils/authentication";
 import { createValidationErrorMiddleware } from "../utils/error";
 import { query } from "express-validator";
 import { getProfileByUsername } from "../database/prisma/profileQueries";
-import { getFriendByUsername } from "../database/prisma/friendQueries";
+import {
+  addFriend,
+  getFriendByUsername,
+} from "../database/prisma/friendQueries";
+import { createCheckProfileByUsernameMiddleware } from "../middlewares/profile";
 
 const friendValidation = {
   usernameQuery: query("username", "Username query is required")
@@ -82,4 +86,74 @@ const user_friendship_status_get = [
   }),
 ];
 
-export { user_friendship_status_get };
+const friendAddPOSTAuthentication = createAuthenticationMiddleware({
+  message: "Failed to send a friend request to the user",
+  errMessage: "You are not authenticated",
+});
+
+const friendAddPOSTValidationError = createValidationErrorMiddleware({
+  message: "Failed to send a friend request to the user",
+});
+
+const friendAddPOSTProfileNotFound = createCheckProfileByUsernameMiddleware({
+  message: "Failed to send a friend request to the user",
+});
+
+const friendAddPOSTFriendshipCheck: RequestHandler = async (req, res, next) => {
+  const userUsername = req.user?.username as string;
+  const friendUsername = req.query.username as string;
+
+  const friend = await getFriendByUsername({
+    userUsername,
+    friendUsername,
+  });
+
+  if (friend) {
+    const responseMessages = {
+      ACTIVE: "You are already friends with the user",
+      PENDING: "You already sent a friend request to the user",
+    };
+    const message = responseMessages[friend.status];
+
+    res.status(409).json({
+      message: "Failed to send a friend request to the user",
+      error: {
+        message,
+      },
+    });
+
+    return;
+  }
+
+  next();
+};
+
+const friend_add_post = [
+  friendAddPOSTAuthentication,
+  friendValidation.usernameQuery,
+  friendAddPOSTValidationError,
+  asyncHandler(friendAddPOSTProfileNotFound),
+  asyncHandler(friendAddPOSTFriendshipCheck),
+  asyncHandler(async (req: Request, res: Response) => {
+    const userId = req.user?.id as string;
+    const friendId = req.friend.id as string;
+
+    const { friends, notification } = await addFriend({
+      userId,
+      friendId,
+      payload: "sent a friend request",
+    });
+
+    req.app
+      .get("io")
+      .to(`notification:${notification.triggeredForId}`)
+      .emit("notificationcreate", notification);
+
+    res.json({
+      message: "Successfully sent a friend request to the user",
+      friends,
+    });
+  }),
+];
+
+export { user_friendship_status_get, friend_add_post };
