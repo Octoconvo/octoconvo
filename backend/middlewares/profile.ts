4e2ee5a219471591f1f3495bda05f8d5f9b12026
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-25 18:35:20 +0700

    feat: add a fn to create profile check middleware
    Add a function that create a middleware that fetches user profile by
    username and return 404 response if the profile doesn't exist.

---
+++ backend/middlewares/profile.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from "express";
+import { getProfileByUsername } from "../database/prisma/profileQueries";
+
+const createCheckProfileByUsernameMiddleware = ({
+  message,
+}: {
+  message: string;
+}): RequestHandler => {
+  return async (req, res, next) => {
+    const username = req.query.username as string;
+    const profile = await getProfileByUsername(username);
+
+    if (profile === null) {
+      res.status(404).json({
+        message,
+        error: {
+          message: "User with that username doesn't exist",
+        },
+      });
+
+      return;
+    }
+
+    req.friend = profile;
+    next();
+  };
+};
+
+export { createCheckProfileByUsernameMiddleware };
