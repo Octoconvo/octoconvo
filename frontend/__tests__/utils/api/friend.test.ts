d934895a02c241782b61378cf05403a9d99eb2a5
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-25 19:21:31 +0700

    test: add postFriendToAPI test

--- frontend/__tests__/utils/api/friend.test.ts
+++ frontend/__tests__/utils/api/friend.test.ts
@@ -1,17 +1,36 @@
-import { getFriendshipStatusFromAPI } from "@/utils/api/friend";
+import { FriendAPI } from "@/types/api";
+import {
+  getFriendshipStatusFromAPI,
+  postFriendToAPI,
+} from "@/utils/api/friend";
 
 const message = "Successfully fetched data";
 const friendshipStatus = "ACTIVE";
 
 const DOMAIN_URL = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
+const friend: FriendAPI = {
+  friendOfId: "testfriend1",
+  friendId: "testfriend1",
+  createdAt: new Date().toISOString(),
+  status: "PENDING",
+  updatedAt: new Date().toISOString(),
+};
+
+const friends = [friend];
+
 global.fetch = jest.fn((_url: string) => {
+  const paths = _url.split("/");
+  const lastPath = paths[paths.length - 1];
+  const isFriend = lastPath === "friend";
+
   return Promise.resolve({
     status: 200,
     json: () =>
       Promise.resolve({
         message,
-        friendshipStatus,
+        ...(!isFriend ? { friendshipStatus } : {}),
+        ...(isFriend ? { friends } : {}),
       }),
   });
 }) as jest.Mock;
@@ -43,4 +62,19 @@ describe("Test getFriendshipStatusFromAPI", () => {
       );
     }
   );
+});
+
+describe("Test postFriendToAPI", () => {
+  test("postFriendToAPI return the correct data", async () => {
+    const formData = new URLSearchParams();
+    const postFriendToAPIData = await postFriendToAPI({
+      formData,
+    });
+
+    expect(postFriendToAPIData).toStrictEqual({
+      message,
+      status: 200,
+      friends,
+    });
+  });
 });
