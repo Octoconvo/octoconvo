3e2556e5161438c24e05b32dc9164918e5d02e28
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-26 20:02:00 +0700

    test: test FriendshipStatusButton onClick fn

--- frontend/__tests__/components/Explore/FriendshipStatusButton.test.tsx
+++ frontend/__tests__/components/Explore/FriendshipStatusButton.test.tsx
@@ -1,6 +1,29 @@
 import FriendshipStatusButton from "@/components/Explore/FriendshipStatusButton";
-import { render } from "@testing-library/react";
-import { screen } from "@testing-library/react";
+import { FriendAPI } from "@/types/api";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+const setFriendshipStatusMock = jest.fn();
+const friendUsernameMock = "testusername1";
+
+const friend: FriendAPI = {
+  status: "PENDING",
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+  friendOfId: "testfriend1",
+  friendId: "testfriend1",
+};
+const successObj = {
+  message: "Successfully fetched data",
+  friends: [friend, friend],
+};
+
+global.fetch = jest.fn((_url) => {
+  return Promise.resolve({
+    status: 200,
+    json: () => Promise.resolve().then(() => successObj),
+  });
+}) as jest.Mock;
 
 describe(
   "Render FriendshipStatusButton to test its" + " conditional texts",
@@ -9,7 +32,13 @@ describe(
       "The button should render 'loading...' when the friendship status" +
         " is null",
       () => {
-        render(<FriendshipStatusButton friendshipStatus={null} />);
+        render(
+          <FriendshipStatusButton
+            friendshipStatus={null}
+            setFriendshipStatus={setFriendshipStatusMock}
+            friendUsername={friendUsernameMock}
+          />
+        );
         const friendshipStatusButton = screen.getByTestId(
           "friendship-status-btn"
         ) as HTMLButtonElement;
@@ -21,7 +50,13 @@ describe(
       "The button should render 'Requested' when the friendship status" +
         " is 'PENDING'",
       () => {
-        render(<FriendshipStatusButton friendshipStatus={"PENDING"} />);
+        render(
+          <FriendshipStatusButton
+            friendshipStatus={"PENDING"}
+            setFriendshipStatus={setFriendshipStatusMock}
+            friendUsername={friendUsernameMock}
+          />
+        );
         const friendshipStatusButton = screen.getByTestId(
           "friendship-status-btn"
         ) as HTMLButtonElement;
@@ -33,7 +68,13 @@ describe(
       "The button should render 'Friend' when the friendship status" +
         " is 'ACTIVE'",
       () => {
-        render(<FriendshipStatusButton friendshipStatus={"ACTIVE"} />);
+        render(
+          <FriendshipStatusButton
+            friendshipStatus={"ACTIVE"}
+            setFriendshipStatus={setFriendshipStatusMock}
+            friendUsername={friendUsernameMock}
+          />
+        );
         const friendshipStatusButton = screen.getByTestId(
           "friendship-status-btn"
         ) as HTMLButtonElement;
@@ -45,7 +86,13 @@ describe(
       "The button should render 'Add Friend' when the friendship status" +
         " is 'NONE'",
       () => {
-        render(<FriendshipStatusButton friendshipStatus={"NONE"} />);
+        render(
+          <FriendshipStatusButton
+            friendshipStatus={"NONE"}
+            setFriendshipStatus={setFriendshipStatusMock}
+            friendUsername={friendUsernameMock}
+          />
+        );
         const friendshipStatusButton = screen.getByTestId(
           "friendship-status-btn"
         ) as HTMLButtonElement;
@@ -54,3 +101,42 @@ describe(
     );
   }
 );
+describe("Test FriendshipStatusButton onClick function", () => {
+  const user = userEvent.setup();
+
+  test(
+    "The onClick function won't proceed is the friendshipStatus is not" +
+      " 'NONE'",
+    async () => {
+      render(
+        <FriendshipStatusButton
+          friendshipStatus={null}
+          setFriendshipStatus={setFriendshipStatusMock}
+          friendUsername={friendUsernameMock}
+        />
+      );
+      const friendshipStatusButton = screen.getByTestId(
+        "friendship-status-btn"
+      ) as HTMLButtonElement;
+
+      await user.click(friendshipStatusButton);
+      expect(setFriendshipStatusMock).toHaveBeenCalledTimes(0);
+    }
+  );
+
+  test("The onClick function proceed if the friendshipStatus is 'NONE'", async () => {
+    render(
+      <FriendshipStatusButton
+        friendshipStatus="NONE"
+        setFriendshipStatus={setFriendshipStatusMock}
+        friendUsername={friendUsernameMock}
+      />
+    );
+    const friendshipStatusButton = screen.getByTestId(
+      "friendship-status-btn"
+    ) as HTMLButtonElement;
+
+    await user.click(friendshipStatusButton);
+    expect(setFriendshipStatusMock).toHaveBeenCalledTimes(1);
+  });
+});
