eb91c33646ea84867eb3d25e1dacc23bc3a67773
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-08-28 04:48:41 +0700

    feat(route): add friend request route

--- backend/routes/friend.ts
+++ backend/routes/friend.ts
@@ -7,4 +7,6 @@ router.get("/friendship-status", friendController.user_friendship_status_get);
 
 router.post("", friendController.friend_add_post);
 
+router.post("/request", friendController.friend_request_post);
+
 export default router;
