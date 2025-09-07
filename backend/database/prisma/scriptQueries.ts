a9ae2cd1473e29e88046de473933f0d24a654210
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-07 10:12:35 +0700

    feat: add database queries for friend scripts

---
+++ backend/database/prisma/scriptQueries.ts
@@ -0,0 +1,105 @@
+import { User, FriendsStatus } from "@prisma/client";
+import prisma from "./client";
+
+const getUserByUsername = async (username: string): Promise<User | null> => {
+  return await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+};
+
+type GetSeedUsersData = Promise<User[]>;
+
+const getSeedUsers = async (): GetSeedUsersData => {
+  const user = await prisma.user.findMany({
+    where: {
+      username: {
+        startsWith: "seeduser",
+      },
+    },
+  });
+
+  return user;
+};
+
+const deleteUserFriends = async (userId: string) => {
+  await prisma.friend.deleteMany({
+    where: {
+      OR: [{ friendOfId: userId }, { friendId: userId }],
+    },
+  });
+};
+
+type CreateFriends = {
+  friendOfId: string;
+  friendId: string;
+  status: FriendsStatus;
+};
+
+const createFriend = ({ friendOfId, friendId, status }: CreateFriends) => {
+  return prisma.friend.create({
+    data: {
+      friendOf: {
+        connect: {
+          id: friendOfId,
+        },
+      },
+      friend: {
+        connect: {
+          id: friendId,
+        },
+      },
+      status: status,
+    },
+  });
+};
+
+type CreateFriendsRelationship = {
+  userOneId: string;
+  userTwoId: string;
+  status: FriendsStatus;
+};
+
+const createFriendsRelationship = async ({
+  userOneId,
+  userTwoId,
+  status,
+}: CreateFriendsRelationship) => {
+  const userOneToUserTwo = createFriend({
+    friendOfId: userOneId,
+    friendId: userTwoId,
+    status: status,
+  });
+
+  const userTwoToUserOne = createFriend({
+    friendOfId: userTwoId,
+    friendId: userOneId,
+    status: status,
+  });
+
+  await prisma.$transaction([userOneToUserTwo, userTwoToUserOne]);
+};
+
+const getSeedUsersToPopulateFriends = () => {
+  const users = prisma.user.findMany({
+    where: {
+      username: {
+        startsWith: "seeduser",
+      },
+      NOT: { username: "seeduser1" },
+    },
+    take: 100,
+    orderBy: { username: "asc" },
+  });
+
+  return users;
+};
+
+export {
+  getUserByUsername,
+  getSeedUsers,
+  getSeedUsersToPopulateFriends,
+  deleteUserFriends,
+  createFriendsRelationship,
+};
