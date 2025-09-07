a22bc33158ff25f576abdd361d35e31ceedeed97
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-08 05:48:50 +0700

    refactor: handle await errors in populateFriendsDB
    - Move try catch in a separate generate function to handle async error.
    - Use the name populate on the function that generate the promises.

--- backend/scripts/populateFriendsDB.ts
+++ backend/scripts/populateFriendsDB.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { FriendsStatus, User } from "@prisma/client";
 import {
   createFriendsRelationship,
   getUserByUsername,
@@ -8,30 +8,44 @@ import { isEven } from "../utils/numberUtils";
 import { logErrorMessage } from "../utils/error";
 import { logPopulateMessage } from "../utils/loggerUtils";
 
+type GenerateUserFriend = {
+  user: User;
+  friend: User;
+  status: FriendsStatus;
+};
+
+const generateUserFriend = async ({
+  user,
+  friend,
+  status,
+}: GenerateUserFriend) => {
+  try {
+    logPopulateMessage(
+      `Creating friend relationships between ${user.username} ` +
+        ` and ${friend.username} ...`,
+    );
+    await createFriendsRelationship({
+      userOneId: user.id,
+      userTwoId: friend.id,
+      status,
+    });
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
 type GenerateUserFriends = {
   user: User;
   friends: User[];
 };
 
-const generateUserFriends = async ({ user, friends }: GenerateUserFriends) => {
+const populateUserFriends = async ({ user, friends }: GenerateUserFriends) => {
   const createFriendsPromises = friends.map((friend: User, index: number) => {
     const status = isEven(index + 1) ? "PENDING" : "ACTIVE";
 
     return new Promise(resolve => {
-      try {
-        logPopulateMessage(
-          `Creating friend relationships between ${user.username} ` +
-            ` and ${friend.username} ...`,
-        );
-        createFriendsRelationship({
-          userOneId: user.id,
-          userTwoId: friend.id,
-          status,
-        });
-        resolve(1);
-      } catch (err) {
-        logErrorMessage(err);
-      }
+      generateUserFriend({ user, friend, status });
+      resolve(1);
     });
   });
 
@@ -48,7 +62,7 @@ const populateFriends = async ({ friends }: PopulateFriends) => {
   const seedUserThree: User | null = await getUserByUsername("seeduser3");
 
   if (seedUserOne && seedUserTwo && seedUserThree) {
-    await generateUserFriends({ user: seedUserOne, friends });
+    await populateUserFriends({ user: seedUserOne, friends });
 
     await createFriendsRelationship({
       userOneId: seedUserOne.id,
