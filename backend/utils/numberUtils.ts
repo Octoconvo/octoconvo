2875aa39e9dd28e577cf3997f1316c19979fb81c
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-09 13:22:26 +0700

    feat(util): add getBiggestPowerOfTen function

--- backend/utils/numberUtils.ts
+++ backend/utils/numberUtils.ts
@@ -2,4 +2,20 @@ const isEven = (number: number) => {
   return number % 2 === 0;
 };
 
-export { isEven };
+const getBiggestPowerOfTen = (number: number) => {
+  let currentNumber = 1;
+  let power = 0;
+
+  while (currentNumber < number) {
+    const nextNumber = currentNumber * 10;
+    currentNumber = nextNumber;
+
+    if (nextNumber <= number) {
+      power += 1;
+    }
+  }
+
+  return power;
+};
+
+export { isEven, getBiggestPowerOfTen };
