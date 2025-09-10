f5eb49df063c651b21c5754a88c43885a0e59dd8
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-10 12:28:48 +0700

    test(util): add generateArrayOfInts tests

--- backend/__tests__/utils/testUtils.test.ts
+++ backend/__tests__/utils/testUtils.test.ts
@@ -1,4 +1,7 @@
-import { getValidationErrorMsg } from "../../utils/testUtils";
+import {
+  generateArrayOfInts,
+  getValidationErrorMsg,
+} from "../../utils/testUtils";
 
 describe("Test getValidationErrorMsg function", () => {
   test("Return null if the error object is undefined", () => {
@@ -33,5 +36,27 @@ describe("Test getValidationErrorMsg function", () => {
     });
 
     expect(string).toBe(null);
+  });
+});
+
+describe("Test generateArrayOfInts function", () => {
+  test("Return an array with 20 items", () => {
+    const array = generateArrayOfInts(20);
+    expect(array.length).toBe(20);
+  });
+
+  test("Return [1, 2, 3] if size argument is 3", () => {
+    const array = generateArrayOfInts(3);
+    expect(array).toStrictEqual([1, 2, 3]);
+  });
+
+  test("Return an array with 9999 items", () => {
+    const array = generateArrayOfInts(9999);
+    expect(array.length).toBe(9999);
+  });
+
+  test("The last item should be 123456 if the size is 123456", () => {
+    const array = generateArrayOfInts(123456);
+    expect(array[array.length - 1]).toBe(123456);
   });
 });
