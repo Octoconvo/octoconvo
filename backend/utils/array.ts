f24d7a4f88060c9665c0802cbe0f523c7c5686d3
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-10 12:47:19 +0700

    feat(util): add breakArrayIntoSubarrays function

--- backend/utils/array.ts
+++ backend/utils/array.ts
@@ -2,4 +2,39 @@ const getLastItemInTheArray = <Item>(array: Item[]): Item | undefined => {
   return array[array.length - 1];
 };
 
-export { getLastItemInTheArray };
+const pushEmptyArray = <Type>(array: Type[][]) => {
+  array.push([]);
+};
+
+const isArrayIndexUndefined = <Type>(array: Type[], index: number): boolean => {
+  return array[index] === undefined;
+};
+
+const breakArrayIntoSubArrays = <Type>({
+  array,
+  subArraySize,
+}: {
+  array: Type[];
+  subArraySize: number;
+}) => {
+  const arrayCopy = array;
+  const subArray: Type[][] = [];
+
+  let subArrayIndex = 0;
+
+  for (let i = 0; i < arrayCopy.length; i++) {
+    if (isArrayIndexUndefined(subArray, subArrayIndex)) {
+      pushEmptyArray(subArray);
+    }
+
+    subArray[subArrayIndex].push(arrayCopy[i]);
+
+    if (subArray[subArrayIndex].length === subArraySize) {
+      subArrayIndex++;
+    }
+  }
+
+  return subArray;
+};
+
+export { getLastItemInTheArray, breakArrayIntoSubArrays };
