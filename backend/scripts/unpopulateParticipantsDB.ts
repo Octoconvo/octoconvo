c2c5bc59b032f07455e6ff3fd98b53f6763d3e0b
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-14 15:38:09 +0700

    chore(script): add unpopulateParticipantsDB

---
+++ backend/scripts/unpopulateParticipantsDB.ts
@@ -0,0 +1,51 @@
+import {
+  getSeedCommunities,
+  deleteCommunityParticipants,
+} from "../database/prisma/scriptQueries";
+import { Community } from "@prisma/client";
+import { breakArrayIntoSubArrays } from "../utils/array";
+import { logErrorMessage } from "../utils/error";
+import {
+  logUnpopulateMessage,
+  logUnpopulateSuccessMessage,
+} from "../utils/loggerUtils";
+
+const destroyCommunityParticipants = async (community: Community) => {
+  try {
+    logUnpopulateMessage(`Deleting ${community.name}'s participants`);
+    await deleteCommunityParticipants(community.id);
+    logUnpopulateSuccessMessage(
+      `Successfully deleted ${community.name}'s participants`,
+    );
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
+const unpopulateCommunitiesParticipants = async (communites: Community[]) => {
+  const subCommunitiesArrays = breakArrayIntoSubArrays({
+    array: communites,
+    subArraySize: 10,
+  });
+
+  for (const subCommunitiesArray of subCommunitiesArrays) {
+    const deleteParticipantsPromises = subCommunitiesArray.map(
+      async community => {
+        await destroyCommunityParticipants(community);
+      },
+    );
+
+    await Promise.all(deleteParticipantsPromises);
+  }
+};
+
+const unpopulateParticipantsDB = async () => {
+  try {
+    const communities = await getSeedCommunities();
+    await unpopulateCommunitiesParticipants(communities);
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
+unpopulateParticipantsDB();
