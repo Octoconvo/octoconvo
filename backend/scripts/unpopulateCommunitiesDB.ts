287f2e64c61d452b27d733c871d084c96a260d11
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-15 09:00:47 +0700

    chore(script): add unpopulateCommunitiesDB

---
+++ backend/scripts/unpopulateCommunitiesDB.ts
@@ -0,0 +1,39 @@
+import { Community } from "@prisma/client";
+import {
+  deleteCommunityAndItsData,
+  getSeedCommunities,
+} from "../database/prisma/scriptQueries";
+import { logErrorMessage } from "../utils/error";
+import {
+  logUnpopulateMessage,
+  logUnpopulateSuccessMessage,
+} from "../utils/loggerUtils";
+
+const destroyCommunity = async (community: Community) => {
+  try {
+    logUnpopulateMessage(`\x1b[31mDeleting community ${community.name}...`);
+    await deleteCommunityAndItsData(community.id);
+    logUnpopulateSuccessMessage(
+      `Successfully deleted community ${community.name}`,
+    );
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
+const unpopulateCommunities = async (communities: Community[]) => {
+  for (const community of communities) {
+    destroyCommunity(community);
+  }
+};
+
+const unpopulateCommunitiesDB = async () => {
+  try {
+    const seedCommunities = await getSeedCommunities();
+    await unpopulateCommunities(seedCommunities);
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
+unpopulateCommunitiesDB();
