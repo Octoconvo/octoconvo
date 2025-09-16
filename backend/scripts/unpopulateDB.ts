3e4f06950746c92087f4adbbbc8149d49a4eccec
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-16 10:33:44 +0700

    refactor: improve unpopulateDB script readibility

--- backend/scripts/unpopulateDB.ts
+++ backend/scripts/unpopulateDB.ts
@@ -1,153 +1,90 @@
 #!/usr/bin/env node
-import { Community, Inbox, User } from "@prisma/client";
-import prisma from "../database/prisma/client";
+import { Community, User } from "@prisma/client";
+import {
+  deleteCommunityAndItsData,
+  deleteUser,
+  deleteUserFriends,
+  deleteUserNotifications,
+  getSeedCommunities,
+  getSeedLoneUsers,
+  getSeedUsers,
+} from "../database/prisma/scriptQueries";
+import { logErrorMessage } from "../utils/error";
+import {
+  logUnpopulateMessage,
+  logUnpopulateSuccessMessage,
+} from "../utils/loggerUtils";
+import { createTimer } from "../utils/timeUtils";
+
+const destroyCommunity = async (community: Community) => {
+  try {
+    logUnpopulateMessage(`Deleting ${community.name} with its data...`);
+    await deleteCommunityAndItsData(community.id);
+    logUnpopulateSuccessMessage(
+      `Successfully deleted ${community.name} with its data`,
+    );
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
+const unpopulateCommunities = async (communities: Community[]) => {
+  for (const community of communities) {
+    await destroyCommunity(community);
+  }
+};
+
+const destroyUser = async (user: User) => {
+  try {
+    logUnpopulateMessage(`Deleting ${user.username} with its data...`);
+    await deleteUserNotifications(user.id);
+    await deleteUserFriends(user.id);
+    await deleteUser(user.id);
+    logUnpopulateSuccessMessage(
+      `Successfully deleted ${user.username} with its data`,
+    );
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
+const unpopulateUsers = async (users: User[]) => {
+  for (const user of users) {
+    await destroyUser(user);
+  }
+};
+
+type Unpopulate = {
+  users: User[];
+  loneUsers: User[];
+  communities: Community[];
+};
+
+const unpopulate = async ({ users, loneUsers, communities }: Unpopulate) => {
+  await unpopulateCommunities(communities);
+  await unpopulateUsers(users);
+  await unpopulateUsers(loneUsers);
+};
 
 const unpopulateDB = async () => {
-  const seedUsers = await prisma.user.findMany({
-    where: {
-      username: {
-        startsWith: "seeduser",
-      },
-    },
-  });
-
-  const seedLoneUsers = await prisma.user.findMany({
-    where: {
-      username: {
-        startsWith: "seedloneuser",
-      },
-    },
-  });
-
-  const deleteUserData = async ({ user }: { user: User }) => {
-    console.log(`\x1b[33mDeleting ${user.username}'s associated data`);
-    type CommunityWithInbox = Community & {
-      inbox: Inbox;
-    };
-
-    const deleteUserNotifications = async () => {
-      await prisma.notification.deleteMany({
-        where: {
-          OR: [{ triggeredById: user.id }, { triggeredForId: user.id }],
-        },
-      });
-    };
-
-    await deleteUserNotifications();
-
-    const deleteUserFriends = async () => {
-      await prisma.friend.deleteMany({
-        where: {
-          OR: [{ friendOfId: user.id }, { friendId: user.id }],
-        },
-      });
-    };
-
-    await deleteUserFriends();
-
-    const getUserCommunities = async (): Promise<CommunityWithInbox[]> => {
-      const community = (await prisma.community.findMany({
-        where: {
-          participants: {
-            some: {
-              AND: [{ userId: user?.id }, { role: "OWNER" }],
-            },
-          },
-        },
-        include: {
-          inbox: true,
-        },
-      })) as CommunityWithInbox[];
-
-      return community;
-    };
-
-    const communities: CommunityWithInbox[] = await getUserCommunities();
-
-    const cascadeDeleteCommunityAndItsData = async (
-      community: CommunityWithInbox,
-    ) => {
-      console.log(`\x1b[33mDeleting ${community.name} and its related data`);
-
-      const deleteCommunityNotifications = prisma.notification.deleteMany({
-        where: { communityId: community.id },
-      });
-
-      const deleteCommunityMessages = prisma.message.deleteMany({
-        where: {
-          inboxId: community.inbox?.id as string,
-        },
-      });
-
-      const deleteCommunityParticipants = prisma.participant.deleteMany({
-        where: {
-          communityId: community.id,
-        },
-      });
-
-      const deleteCommunityInboxes = prisma.inbox.deleteMany({
-        where: {
-          communityId: community.id,
-        },
-      });
-
-      const deleteCommunity = prisma.community.deleteMany({
-        where: {
-          id: community.id,
-        },
-      });
-
-      await prisma.$transaction([
-        deleteCommunityNotifications,
-        deleteCommunityMessages,
-        deleteCommunityParticipants,
-        deleteCommunityInboxes,
-        deleteCommunity,
-      ]);
-    };
-
-    // Delete communities and their related data
-    for (const community of communities) {
-      await cascadeDeleteCommunityAndItsData(community);
-      console.log(
-        `\x1b[33mFinished deleting ${community.name}'s` +
-          " associated community",
-      );
-    }
-  };
-
-  const deleteUser = async ({ username }: { username: string }) => {
-    console.log(`\x1b[31mDeleting user ${username}.`);
-
-    await prisma.user.delete({
-      where: {
-        username: username,
-      },
-    });
-  };
-
+  logUnpopulateMessage("Unpopulating database...");
+  const timer = createTimer();
   try {
-    for (const user of seedUsers) {
-      try {
-        await deleteUserData({ user });
-      } catch (err) {
-        if (err instanceof Error) {
-          console.log(err.message);
-        }
-      }
-    }
-
-    for (const user of seedUsers) {
-      await deleteUser({ username: user.username });
-    }
-
-    for (const seedLoneUser of seedLoneUsers) {
-      await deleteUser({ username: seedLoneUser.username });
-    }
+    const seedUsers = await getSeedUsers();
+    const seedLoneUsers = await getSeedLoneUsers();
+    const seedCommunities = await getSeedCommunities();
+    await unpopulate({
+      users: seedUsers,
+      loneUsers: seedLoneUsers,
+      communities: seedCommunities,
+    });
   } catch (err) {
-    if (err instanceof Error) {
-      console.log(err.message);
-    }
+    logErrorMessage(err);
+  } finally {
+    timer.stopTimer();
+    const timeInSecond = timer.timeInSecond;
+    logUnpopulateSuccessMessage("Finished unpopulating database");
+    logUnpopulateSuccessMessage(`${timeInSecond} s`);
   }
 };
 
