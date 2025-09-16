3f8ef2b48c15d1a73087f8dde4f73b0bc8736783
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-16 09:55:28 +0700

    refactor: add populateUsersDB and use it in script

--- backend/scripts/populateDB.ts
+++ backend/scripts/populateDB.ts
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
-import prisma from "../database/prisma/client";
-import bcrypt from "bcrypt";
 import { populateFriendsDB } from "./populateFriendsScript";
 import { populateMessagesDB } from "./populateMessagesScript";
-import { generateArrayOfSeedUsers } from "../utils/scriptUtils";
 import { createTimer } from "../utils/timeUtils";
 import { populateNotificationsDB } from "./populateNotificationsScript";
 import { populateParticipantsDB } from "./populateParticipantsScript";
 import { populateCommunitiesDB } from "./populateCommunitiesScript";
-import { generateSeedLoneUser } from "./populateUsersScript";
+import { populateUsersDB } from "./populateUsersScript";
+import {
+  logPopulateMessage,
+  logPopulateSuccessMessage,
+} from "../utils/loggerUtils";
 
 type Mode = "COMPACT" | "BALANCED" | "EXTENSIVE";
 
@@ -22,58 +23,18 @@ const modeSizes = {
 
 // Populate database for testing purposes
 const populateDB = async (size: number) => {
-  console.log(`\x1b[36mPopulating database...`);
+  logPopulateMessage("Populating database...");
   const timer = createTimer();
-
-  try {
-    const seedUsers = generateArrayOfSeedUsers(size);
-
-    const populateDatabaseWithSeedData = async () => {
-      const createSeedUserAndItsData = seedUsers.map(async seedUser => {
-        return new Promise((resolve): void =>
-          bcrypt.hash(seedUser.password, 10, async (err, hashedPassword) => {
-            if (err) {
-              if (err instanceof Error) console.log(err.message);
-            }
-
-            const createUser = async () => {
-              const user = await prisma.user.create({
-                data: {
-                  username: seedUser.username,
-                  displayName: seedUser.username,
-                  password: hashedPassword,
-                },
-              });
-
-              console.log(`\x1b[36mCreated user ${user.username}...`);
-
-              return user;
-            };
-
-            await createUser();
-            resolve(1);
-          }),
-        );
-      });
-
-      await Promise.all(createSeedUserAndItsData);
-    };
-
-    await populateDatabaseWithSeedData();
-    await generateSeedLoneUser({ index: 1 });
-    await populateCommunitiesDB(size);
-    await populateParticipantsDB(size);
-    await populateMessagesDB();
-    await populateNotificationsDB();
-    await populateFriendsDB();
-  } catch (err) {
-    if (err instanceof Error) console.log(err.message);
-  } finally {
-    timer.stopTimer();
-    const timeInSecond = timer.timeInSecond;
-    console.log(`\x1b[32mFinished populating database`);
-    console.log(`\x1b[32m${timeInSecond} s`);
-  }
+  await populateUsersDB(size);
+  await populateCommunitiesDB(size);
+  await populateParticipantsDB(size);
+  await populateMessagesDB();
+  await populateNotificationsDB();
+  await populateFriendsDB();
+  timer.stopTimer();
+  const timeInSecond = timer.timeInSecond;
+  logPopulateSuccessMessage("Finished populating database");
+  logPopulateSuccessMessage(`${timeInSecond} s`);
 };
 
 const size = modeSizes[mode];
--- backend/scripts/populateUsersScript.ts
+++ backend/scripts/populateUsersScript.ts
@@ -1,12 +1,21 @@
 import bcrypt from "bcrypt";
 import { logErrorMessage } from "../utils/error";
-import { createSeedLoneUser } from "../database/prisma/scriptQueries";
+import {
+  createSeedLoneUser,
+  createUser,
+} from "../database/prisma/scriptQueries";
+import { SeedUserGenerator } from "../@types/scriptTypes";
+import { generateArrayOfSeedUsers } from "../utils/scriptUtils";
+import {
+  logPopulateMessage,
+  logPopulateSuccessMessage,
+} from "../utils/loggerUtils";
 
 type GenereateSeedLoneUser = {
   index: number;
 };
 
-const generateSeedLoneUser = ({ index }: GenereateSeedLoneUser) => {
+const generateSeedLoneUser = async ({ index }: GenereateSeedLoneUser) => {
   bcrypt.hash(`seedlone@User${index}`, 10, async (err, hashedPassword) => {
     if (err) {
       logErrorMessage(err);
@@ -19,4 +28,55 @@ const generateSeedLoneUser = ({ index }: GenereateSeedLoneUser) => {
   });
 };
 
-export { generateSeedLoneUser };
+type GenerateSeedUser = {
+  username: string;
+  displayName: string;
+  password: string;
+};
+
+const generateSeedUser = async ({
+  username,
+  displayName,
+  password,
+}: GenerateSeedUser) => {
+  try {
+    logPopulateMessage(`Creating ${username}...`);
+    await createUser({ username, displayName, password });
+    logPopulateSuccessMessage(`Successfully created ${username}`);
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
+const populateUsers = async (users: SeedUserGenerator[]) => {
+  const createUsersPromises = users.map(user => {
+    return bcrypt.hash(user.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        logErrorMessage(err);
+      } else {
+        await generateSeedUser({
+          username: user.username,
+          displayName: user.displayName,
+          password: hashedPassword,
+        });
+      }
+    });
+  });
+
+  await Promise.all(createUsersPromises);
+};
+
+const populateUsersDB = async (size: number) => {
+  const seedUsersGenerators = generateArrayOfSeedUsers(size);
+
+  try {
+    await populateUsers(seedUsersGenerators);
+    await generateSeedLoneUser({
+      index: 1,
+    });
+  } catch (err) {
+    logErrorMessage(err);
+  }
+};
+
+export { populateUsersDB };
