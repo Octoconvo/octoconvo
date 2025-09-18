b51ca78bf062c45bac29841f6416c769f60e5b50
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-18 12:40:07 +0700

    refactor: use pino in loggerUtils

--- backend/utils/loggerUtils.ts
+++ backend/utils/loggerUtils.ts
@@ -1,17 +1,45 @@
+import pino from "pino";
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV?.toLowerCase() == "development";
+
+const logger = pino(
+  isDev
+    ? {
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorarize: true,
+          },
+        },
+      }
+    : {},
+);
+
+const ANSI = {
+  lightGreen: `\x1b[38:5:190m`,
+  green: `\x1b[38:5:119m`,
+  orange: `\x1b[38:5:208m`,
+  brightRed: `\x1b[38:5:196m`,
+  red: `\x1b[38:5:160m`,
+  reset: `\x1b[0m`,
+  bold: `\x1b[1]`,
+};
+
 const logPopulateMessage = (message: string) => {
-  console.log(`\x1b[36m${message}`);
-};
-
-const logUnpopulateMessage = (message: string) => {
-  console.log(`\x1b[33m${message}`);
-};
-
-const logUnpopulateSuccessMessage = (message: string) => {
-  console.log(`\x1b[31m${message}`);
+  logger.info(`${ANSI.lightGreen}${message}${ANSI.reset}`);
 };
 
 const logPopulateSuccessMessage = (message: string) => {
-  console.log(`\x1b[32m${message}`);
+  logger.info(`${ANSI.green}${ANSI.bold}${message}${ANSI.reset}`);
+};
+
+const logUnpopulateMessage = (message: string) => {
+  logger.info(`${ANSI.orange}${message}${ANSI.reset}`);
+};
+
+const logUnpopulateSuccessMessage = (message: string) => {
+  logger.info(`${ANSI.brightRed}${ANSI.bold}${message}${ANSI.reset}`);
 };
 
 export {
