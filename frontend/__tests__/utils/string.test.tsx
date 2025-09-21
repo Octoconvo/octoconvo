382643de08cf982914c57745b9272979936e7eff
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-21 18:31:25 +0700

    test: add hasPath test

--- frontend/__tests__/utils/string.test.tsx
+++ frontend/__tests__/utils/string.test.tsx
@@ -2,6 +2,7 @@ import {
   createHTMLNewLine,
   unescapeString,
   capitaliseStringFirstLetter,
+  hasPath,
 } from "@/utils/string";
 
 test("Test unescape input return value", () => {
@@ -42,4 +43,26 @@ test("Test capitaliseStringFirstLetter function", () => {
   const string = capitaliseStringFirstLetter("ECONOMY");
 
   expect(string).toBe("Economy");
+});
+
+describe("Test hasPath function", () => {
+  test("Return false if the path is an empty string", () => {
+    expect(hasPath({ path: "", toCompare: "testtocompare" })).toBe(false);
+  });
+
+  test("Return false if the toCompare is an empty string", () => {
+    expect(hasPath({ path: "testpath", toCompare: "" })).toBe(false);
+  });
+
+  test("Return false if the toCompare and path are an empty strings", () => {
+    expect(hasPath({ path: "testpath", toCompare: "" })).toBe(false);
+  });
+
+  test("Return true if the path contains toCompare path", () => {
+    const hasToComparePath = hasPath({
+      path: "/testpath/testtocompare/",
+      toCompare: "testtocompare",
+    });
+    expect(hasToComparePath).toBe(true);
+  });
 });
