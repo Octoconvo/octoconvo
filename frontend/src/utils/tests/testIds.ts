345639a618aacc8030c794004456b063f9a306a3
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-27 22:27:24 +0700

    chore: add friend-list-modal testId

--- frontend/src/utils/tests/testIds.ts
+++ frontend/src/utils/tests/testIds.ts
@@ -1,6 +1,7 @@
 const testIds = {
   friendItemUsername: "friend-item-username",
   friendItemDisplayName: "friend-item-displayName",
+  friendListModal: "friend-list-modal",
 };
 
 export default testIds;
