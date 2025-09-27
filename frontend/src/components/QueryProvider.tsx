c7632b8c2b4c8ff55ef0a413ccb165489fbf15c7
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-09-27 22:26:09 +0700

    feat(component): add QueryProvider component

---
+++ frontend/src/components/QueryProvider.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+const QueryProvider: FC<QueryProviderProps> = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
