78edb4d26921be3bcd5ac90e0bd553ee5633d1ab
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-02 20:20:06 +0700

    feat(util): add closeOnOutsideClick function

--- frontend/src/utils/HTMLUtils.tsx
+++ frontend/src/utils/HTMLUtils.tsx
@@ -18,4 +18,29 @@ const createHTMLNewLine = (input: string) => {
   return <>{content}</>;
 };
 
-export { createHTMLNewLine };
+interface CloseModal {
+  (): void;
+}
+
+interface CloseOnOutsideClickArgs {
+  e: React.MouseEvent;
+  ref: null | React.RefObject<null | HTMLElement>;
+  closeModal: CloseModal;
+}
+
+const closeOnOutsideClick = ({
+  e,
+  ref,
+  closeModal,
+}: CloseOnOutsideClickArgs) => {
+  if (ref === null || ref.current === null) {
+    return;
+  }
+
+  const isChildren = ref?.current?.contains(e.target as HTMLElement);
+  if (!isChildren && ref?.current !== e.target) {
+    closeModal();
+  }
+};
+
+export { createHTMLNewLine, closeOnOutsideClick };
