c0766b95863a040adda0fcc5079bbb98770c4879
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-10-02 17:12:25 +0700

    chore: add friendListModalInfiniteLoader testId

--- frontend/src/utils/tests/testIds.ts
+++ frontend/src/utils/tests/testIds.ts
@@ -2,6 +2,7 @@ const testIds = {
   friendItemUsername: "friend-item-username",
   friendItemDisplayName: "friend-item-displayName",
   friendListModal: "friend-list-modal",
+  friendListModalInfiniteLoader: "friend-list-modal-infinite-loader",
 };
 
 export default testIds;
