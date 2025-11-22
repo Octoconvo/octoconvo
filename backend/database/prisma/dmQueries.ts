530016f0686084d5d964144d0748febcf70b5a62
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-23 01:42:47 +0700

    feat(query): update getUserDMs query return data

--- backend/database/prisma/dmQueries.ts
+++ backend/database/prisma/dmQueries.ts
@@ -1,19 +1,47 @@
-import { DirectMessage } from "@prisma/client";
+import { PrismaPromise } from "@prisma/client";
+import { UserDMData } from "../../@types/database";
 import prisma from "./client";
 
 interface GetUserDMsArgs {
   userId: string;
 }
 
-const getUserDMs = async ({
+const getUserDMs = ({
   userId,
-}: GetUserDMsArgs): Promise<DirectMessage[]> => {
+}: GetUserDMsArgs): PrismaPromise<UserDMData[]> => {
   return prisma.directMessage.findMany({
     where: {
       participants: {
         some: {
           userId: userId,
           status: "ACTIVE",
+        },
+      },
+    },
+    include: {
+      participants: {
+        where: {
+          user: {
+            NOT: {
+              id: userId,
+            },
+          },
+        },
+        select: {
+          id: true,
+          user: {
+            select: {
+              id: true,
+              username: true,
+              displayName: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      inbox: {
+        select: {
+          id: true,
         },
       },
     },
