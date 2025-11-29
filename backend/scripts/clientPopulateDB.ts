cfa7fcd660c84ada4aefb0ee33e21a766bdcf84e
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-11-29 07:18:42 +0700

    feat(script): populate client users' DMs

--- backend/scripts/clientPopulateDB.ts
+++ backend/scripts/clientPopulateDB.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { User } from "@prisma/client";
+import { Inbox, User } from "@prisma/client";
 import bcrypt from "bcrypt";
 import {
   logErrorMessage,
@@ -10,12 +10,110 @@ import { generateClientUserGenerators } from "../utils/scriptUtils";
 import {
   createCommunity,
   createCommunityMessage,
+  createDirectMessage,
+  createDirectMessageMessage,
   createUser,
   getClientUsers,
   getCommunityWithOwnerAndInboxByName,
+  getDirectMessageInboxById,
 } from "../database/prisma/scriptQueries";
 import { createTimer, pause } from "../utils/timeUtils";
 import { ClientUserGenerator } from "../@types/scriptTypes";
+
+interface GenerateDirectMessageMessage {
+  directMessageId: string;
+  userOne: User;
+  userTwo: User;
+  authorId: string;
+  messageCount: number;
+}
+
+const generateDirectMessageMessages = async ({
+  directMessageId,
+  userOne,
+  userTwo,
+  authorId,
+  messageCount,
+}: GenerateDirectMessageMessage) => {
+  const inbox: Inbox | null = await getDirectMessageInboxById(directMessageId);
+
+  const messageInflection: string = messageCount > 1 ? "messages" : "message";
+  logPopulateMessage(
+    `Creating ${messageCount} ${messageInflection} for the direct message` +
+      ` between ${userOne.username} and ${userTwo.username}`,
+  );
+
+  if (inbox) {
+    for (let i: number = 1; i <= messageCount; i++) {
+      await createDirectMessageMessage({
+        number: i,
+        inboxId: inbox.id,
+        authorId: authorId,
+      });
+    }
+  }
+
+  logPopulateSuccessMessage(
+    `Successfully created ${messageCount} ${messageInflection} for the direct` +
+      ` message between ${userOne.username} and ${userTwo.username}`,
+  );
+};
+
+type GenerateClientDirectMessages = {
+  clientUsers: User[];
+  clientUserGenerators: ClientUserGenerator[];
+};
+
+const generateClientDirectMessages = ({
+  clientUsers,
+}: GenerateClientDirectMessages) => {
+  return clientUsers.map(async (userOne: User, indexOne: number) => {
+    clientUsers.map(async (userTwo: User, indexTwo: number): Promise<void> => {
+      if (indexTwo > indexOne) {
+        logPopulateMessage(
+          `Creating a direct message between ${userOne.username}'s ` +
+            ` ${userTwo.username}...`,
+        );
+
+        const directMessage = await createDirectMessage({
+          userOneId: userOne.id,
+          userTwoId: userTwo.id,
+        });
+
+        logPopulateSuccessMessage(
+          `Successfully created a direct message between` +
+            ` ${userOne.username}'s community ${userTwo.username}`,
+        );
+
+        await generateDirectMessageMessages({
+          directMessageId: directMessage.id,
+          userOne,
+          userTwo,
+          authorId: userOne.id,
+          messageCount: 100,
+        });
+      }
+    });
+  });
+};
+
+type PopulateClientDirectMessages = {
+  clientUsers: User[];
+  clientUserGenerators: ClientUserGenerator[];
+};
+
+const populateClientDirectMessages = async ({
+  clientUsers,
+  clientUserGenerators,
+}: PopulateClientDirectMessages) => {
+  if (doArraysHaveEqualLength(clientUsers, clientUserGenerators)) {
+    await Promise.all(
+      generateClientDirectMessages({ clientUsers, clientUserGenerators }),
+    );
+  } else {
+    throw new Error("arrays have different lengths");
+  }
+};
 
 type GenerateClientUser = {
   username: string;
@@ -169,6 +267,7 @@ const populateClientDB = async () => {
     const clientUserGenerators = generateClientUserGenerators(2);
     const clientUsers = await populateClientUsers(clientUserGenerators);
     await populateClientCommunities({ clientUserGenerators, clientUsers });
+    await populateClientDirectMessages({ clientUserGenerators, clientUsers });
   } catch (err) {
     logErrorMessage(err);
   } finally {
