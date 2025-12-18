178b6ef26f591b326ed04ea805f89a8cca5a6c28
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-12-19 05:31:17 +0700

    test(component): add DirectMessageList test

---
+++ frontend/__tests__/components/DirectMessages/DirectMessageList.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import DirectMessageList from "@/components/DirectMessages/DirectMessageList";
+import { generateDirectMessageMock } from "@/mocks/API/directMessage";
+import { DirectMessageAPI } from "@/types/api";
+
+const directMessages: DirectMessageAPI[] = generateDirectMessageMock(10);
+
+describe("Render RenderMessageList", () => {
+  test(
+    "Render 10 direct messages component when the directMessages.length is" +
+      " 10",
+    () => {
+      render(<DirectMessageList directMessages={directMessages} />);
+      const lists = screen.getAllByRole("listitem");
+      expect(lists.length).toBe(10);
+    }
+  );
+
+  test(
+    "Render zero message information when the directMessages.length is" + " 0",
+    () => {
+      render(<DirectMessageList directMessages={[]} />);
+      const message = screen.getByText("No messages yet.");
+      expect(message).toBeDefined();
+    }
+  );
+});
