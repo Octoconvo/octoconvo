d3c24c62b247abb8dfba92a9476b9fcaaaf78b23
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2025-12-22 05:24:00 +0700

    feat(route): implement DirectMessages path

---
+++ frontend/src/app/lobby/direct-messages/layout.tsx
@@ -0,0 +1,9 @@
+import DirectMessages from "@/components/DirectMessages/DirectMessages";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <DirectMessages />;
+}
