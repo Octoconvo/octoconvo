2a3318b67fa5fc1f2f8dcdbc54447e9ff6431c78
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2026-01-01 23:22:20 +0700

    test(controller): add dm get controller test

--- backend/__tests__/controllers/dm.test.ts
+++ backend/__tests__/controllers/dm.test.ts
@@ -1,10 +1,12 @@
 import request from "supertest";
 import app from "../../config/testConfig";
 import TestAgent from "supertest/lib/agent";
-import { login } from "../../utils/testUtils";
+import { getValidationErrorMsg, login } from "../../utils/testUtils";
 import { Response } from "supertest";
-import { DirectMessage } from "@prisma/client";
+import { DirectMessage, Inbox } from "@prisma/client";
 import { UserDMsGETResponse } from "../../@types/apiResponse";
+import { getDMByParticipants } from "../../database/prisma/testQueries";
+import { ErrorResponse } from "../../@types/error";
 
 describe("Test user DMs get controller", () => {
   test(
@@ -86,6 +88,90 @@ describe("Test user DMs get controller", () => {
       .expect((res: Response) => {
         const directMessages: DirectMessage[] = res.body.directMessages;
         expect(directMessages.length).toBe(1);
+      })
+      .expect(200, done);
+  });
+});
+
+describe("Test DM get controller", () => {
+  let DM:
+    | null
+    | (DirectMessage & {
+        inbox: Inbox | null;
+      }) = null;
+
+  beforeAll(async () => {
+    DM = await getDMByParticipants({
+      usernameOne: "seeduser1",
+      usernameTwo: "seeduser11",
+    });
+  });
+
+  test(
+    "Return 401 unauthorized error when accessing DM without being" +
+      " authenticated",
+    done => {
+      request(app)
+        .get(`/direct-message/${DM?.id}`)
+        .expect("Content-Type", /json/)
+        .expect({
+          message: "Failed to fetch DM with that id",
+          error: {
+            message: "You are not authenticated",
+          },
+        })
+        .expect(401, done);
+    },
+  );
+
+  const agent: TestAgent = request.agent(app);
+  login(agent, {
+    username: "seeduser1",
+    password: "seed@User1",
+  });
+
+  test("Return 422 error when DM id is invalid", done => {
+    agent
+      .get("/direct-message/testdmid")
+      .expect("Content-Type", /json/)
+      .expect((res: Response) => {
+        const message: string = res.body.message;
+        const validationError: ErrorResponse = res.body.error;
+
+        expect(message).toBe("Failed to fetch DM with that id");
+        expect(
+          getValidationErrorMsg({
+            field: "directmessageid",
+            error: validationError,
+          }),
+        ).toBe("DM id is invalid");
+      })
+      .expect(422, done);
+  });
+
+  test("Return 404 error when DM is not found", done => {
+    agent
+      .get(`/direct-message/${DM?.inbox?.id}`)
+      .expect("Content-Type", /json/)
+      .expect({
+        message: "Failed to fetch DM with that id",
+        error: {
+          message: "Can't find DM with that id",
+        },
+      })
+      .expect(404, done);
+  });
+
+  test("Successfully fetched DM after successful validation", done => {
+    agent
+      .get(`/direct-message/${DM?.id}`)
+      .expect("Content-Type", /json/)
+      .expect((res: Response) => {
+        const msg: string = res.body.message;
+        const DM = res.body.directMessage;
+
+        expect(msg).toBe("Successfully fetched DM with that id");
+        expect(DM).toBeDefined();
       })
       .expect(200, done);
   });
