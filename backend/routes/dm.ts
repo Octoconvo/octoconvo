592dc0bd3a072153536dcd65be52ce5125aaee4b
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2026-01-15 16:56:56 +0700

    feat(route): add dm messages GET route

--- backend/routes/dm.ts
+++ backend/routes/dm.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import * as dmController from "../controllers/dm";
+import * as messageController from "../controllers/message";
 
 const router = express.Router();
 
 router.get("/:directmessageid", dmController.DM_get);
+
+router.get("/:directmessageid/messages", messageController.dm_messages_get);
 
 export default router;
