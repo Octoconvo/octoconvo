747f343aec8f456818f88572ba1b1162347e8898
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2026-01-18 16:16:35 +0700

    feat(mock): add MessageMock class

---
+++ frontend/src/mocks/API/message.ts
@@ -0,0 +1,67 @@
+import { InboxMessageAPI, AttachmentAPI } from "@/types/api";
+import { randomUUID } from "crypto";
+import AttachmentMock from "./attachment";
+
+interface MessageMockConstructor {
+  content: string;
+  authorId?: string;
+  authorAvatar?: string | null;
+  authorUsername: string;
+  authorDisplayName: string;
+  replyToId?: boolean | string;
+  attachments?: AttachmentMock[];
+}
+
+class MessageMock implements InboxMessageAPI {
+  id: string;
+  inboxId: string;
+  authorId: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  isDeleted: boolean;
+  isRead: boolean;
+  hiddenFromAuthor: boolean;
+  hiddenFromRecipient: boolean;
+  replyToId: string | null;
+  author: { username: string; displayName: string; avatar: string | null };
+  attachments: AttachmentMock[];
+
+  constructor({
+    content,
+    replyToId,
+    authorUsername,
+    authorAvatar,
+    authorDisplayName,
+    attachments,
+    authorId,
+  }: MessageMockConstructor) {
+    this.id = randomUUID();
+    this.inboxId = randomUUID();
+    this.authorId = authorId ? authorId : randomUUID();
+    this.content = content;
+    this.createdAt = new Date().toISOString();
+    this.updatedAt = this.createdAt;
+    this.isDeleted = false;
+    this.isRead = false;
+    this.hiddenFromAuthor = false;
+    this.hiddenFromRecipient = false;
+    this.replyToId = replyToId
+      ? typeof replyToId === "string"
+        ? replyToId
+        : randomUUID()
+      : null;
+    this.author = {
+      username: authorUsername,
+      displayName: authorDisplayName,
+      avatar: authorAvatar ? authorAvatar : null,
+    };
+    this.attachments = attachments || [];
+
+    for (const attachment of this.attachments) {
+      attachment.updateMessageId(this.id);
+    }
+  }
+}
+
+export default MessageMock;
