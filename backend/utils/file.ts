1971a6eacb0e6a1c1d06d9be87004a13d980101c
Adhisafdhol <103340711+Adhisafdhol@users.noreply.github.com>
2026-01-26 22:25:40 +0700

    feat(util): add uploadFileAndGetUrl function

--- backend/utils/file.ts
+++ backend/utils/file.ts
@@ -1,5 +1,6 @@
 import crypto from "crypto";
 import path from "path";
+import { uploadFile, getPublicURL } from "../database/supabase/supabaseQueries";
 
 const convertFileName = (file: Express.Multer.File): Express.Multer.File => {
   const uuid = crypto.randomUUID();
@@ -9,4 +10,29 @@ const convertFileName = (file: Express.Multer.File): Express.Multer.File => {
   return { ...file, originalname: updatedName };
 };
 
-export { convertFileName };
+interface UploadFileAndGetUrlArgs {
+  file: Express.Multer.File;
+  folder: string;
+  bucketName: string;
+}
+
+const uploadFileAndGetUrl = async ({
+  file,
+  folder,
+  bucketName,
+}: UploadFileAndGetUrlArgs): Promise<{ publicUrl: string }> => {
+  const fileData = await uploadFile({
+    file: file,
+    bucketName: bucketName,
+    folder: folder,
+  });
+
+  const fileUrl = getPublicURL({
+    path: fileData.path,
+    bucketName: bucketName,
+  });
+
+  return fileUrl;
+};
+
+export { convertFileName, uploadFileAndGetUrl };
